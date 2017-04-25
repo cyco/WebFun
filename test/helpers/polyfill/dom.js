87ff555ad91a15622c5d99f9f796a540bdcf95cf
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-04-25 19:39:38 +0200

    WIP

--- test/helpers/polyfill/dom.js
+++ test/helpers/polyfill/dom.js
@@ -3,6 +3,7 @@ import { jsdom } from 'jsdom';
 const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
 global.doc = jsdom(documentHTML);
 global.window = global.doc.defaultView;
+global.document = global.window.document;
 global.Element = global.window.Element;
 global.Image = global.window.Image;
 global.NodeList = global.window.NodeList;
