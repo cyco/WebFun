ec899e887ffd834e49c9ae542a6239f7569da5d8
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-05-26 19:52:06 +0200

    Implement paint bucket tool.

--- src/editor/editors/zone/tools/paint-bucket-tool.js
+++ src/editor/editors/zone/tools/paint-bucket-tool.js
@@ -9,6 +9,72 @@ export default class extends Tool {
 		return 'paint-bucket';
 	}
 
-	activate() {}
-	deactivate() {}
+	constructor() {
+		super();
+
+		this._tile = null;
+	}
+
+	mouseDownAt(x, y, event) {
+		const z = this._editor.currentLayer;
+		const currentTile = this._editor.zone.getTile(x, y, z);
+
+		const NEW = 0;
+		const MARKED = 1;
+		const VISITED = 2;
+
+		const width = this._editor.zone.width;
+		const height = this._editor.zone.height;
+		const size = width * height;
+		const maxX = width - 1;
+		const maxY = height - 1;
+		const state = new Uint8Array(size);
+
+		const open = [
+			x + width * y
+		];
+
+		const neighbors = (i) => {
+			const result = new Array(4);
+
+			const x = i % width;
+			const y = Math.floor(i / width);
+
+			if (x > 0) result.push(i - 1);
+			if (x < maxX) result.push(i + 1);
+			if (y > 0) result.push(i - width);
+			if (y < maxY) result.push(i + width);
+
+			return result;
+		};
+
+		const isCandidate = (i) => this._editor.zone.getTile(i % width, Math.floor(i / width), z) === currentTile;
+		const visit = (i) => {
+			if (state[i] !== NEW) return;
+
+			if (isCandidate(i)) {
+				state[i] = MARKED;
+				open.push(i);
+			} else state[i] = VISITED;
+		};
+
+		visit(open[0]);
+
+		let current;
+		while ((current = open.shift()) !== undefined) {
+			neighbors(current).forEach(visit);
+		}
+
+		const tile = this._editor.currentTile;
+		const zone = this._editor.zone;
+		state.forEach((i, idx) => i === MARKED && zone.setTile(tile, idx % width, Math.floor(idx / width), z));
+	}
+
+	set tile(t) {
+		this._tile = t;
+	}
+
+	get tile() {
+		return this._tile;
+	}
 }
