af31c25a239960158666a99ac96468e62ec29686
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-05-26 20:55:21 +0200

    Implement a tile picker tool.

--- src/editor/editors/zone/tools/index.js
+++ src/editor/editors/zone/tools/index.js
@@ -2,3 +2,4 @@ export MoveTool from './move-tool';
 export BrushTool from './brush-tool';
 export PaintBucketTool from './paint-bucket-tool';
 export RectangleTool from './rectangle-tool';
+export TilePickerTool from './tile-picker-tool';
