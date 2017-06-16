e7ea00c808154b31b622a3aa88ada7f771f8666f
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-06-16 17:28:09 +0200

    Adjust configuration files to incoperate font loader changes

--- webpack.config.js
+++ webpack.config.js
@@ -20,43 +20,47 @@ module.exports = {
 	},
 	module: {
 		rules: [{
-			/* JavaScript / Babel */
-			test: /\.js?$/,
-			loader: "babel-loader",
-			include: [
-				Path.resolve(__dirname, "src")
-			],
-			exclude: [
-				'node_modules',
-				Path.resolve(__dirname, "src/editor"),
-				Path.resolve(__dirname, "src/debug")
-			]
-		}, {
-			/* JavaScript / Babel */
-			test: /\.js?$/,
-			loader: "babel-loader",
-			include: [
-				Path.resolve(__dirname, "src/editor"),
-				Path.resolve(__dirname, "src/debug"),
-				Path.resolve(__dirname, "test/helpers")
-			]
-		}, {
-			/* Styles */
-			test: /\.scss$/,
-			use: [{
-				loader: "style-loader"
+				/* JavaScript / Babel */
+				test: /\.js?$/,
+				loader: "babel-loader",
+				include: [
+					Path.resolve(__dirname, "src")
+				],
+				exclude: [
+					'node_modules',
+					Path.resolve(__dirname, "src/editor"),
+					Path.resolve(__dirname, "src/debug")
+				]
 			}, {
-				loader: "css-loader"
+				/* JavaScript / Babel */
+				test: /\.js?$/,
+				loader: "babel-loader",
+				include: [
+					Path.resolve(__dirname, "src/editor"),
+					Path.resolve(__dirname, "src/debug"),
+					Path.resolve(__dirname, "test/helpers")
+				]
 			}, {
-				loader: "sass-loader",
-				options: {
-					includePaths: ["src/_style"]
-				}
-			}],
-			exclude: [
-				'node_modules'
-			]
-		}]
+				/* Styles */
+				test: /\.scss$/,
+				use: [{
+					loader: "style-loader"
+				}, {
+					loader: "css-loader"
+				}, {
+					loader: "sass-loader",
+					options: {
+						includePaths: ["src/_style"]
+					}
+				}],
+				exclude: [
+					'node_modules'
+				]
+			},
+			/** fonts **/
+			{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+			{ test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+		]
 	},
 	cache: true,
 	plugins: [
