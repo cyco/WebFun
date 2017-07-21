4ef94da1bde09459d48e16ae3bd946e27bea21d0
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-07-21 22:28:33 +0200

    Remove declarative parser

--- src/engine/data-format/setup-image.js
+++
@@ -1,9 +0,0 @@
-import { uint8, uint32, structure, array } from "/parser";
-
-const IMAGE_WIDTH = 288;
-const IMAGE_HEIGHT = 288;
-
-export const setupImage = structure({
-	size: uint32,
-	pixelData: array(uint8, IMAGE_WIDTH * IMAGE_HEIGHT)
-});
