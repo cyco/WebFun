c2767d3e4ba9fc1b6819b91fa719990829ea459e
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-07-25 21:22:28 +0200

    Implement difficulty window

--- src/app/windows/index.js
+++ src/app/windows/index.js
@@ -2,3 +2,4 @@ export MainMenu from "./main-menu";
 export MainWindow from "./main-window";
 export StatisticsWindow from "./statistics-window";
 export GameSpeedWindow from "./game-speed-window";
+export DifficultyWindow from "./difficulty-window";
