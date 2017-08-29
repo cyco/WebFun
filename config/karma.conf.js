b3609e00f47c5328c9c5ced3ff4d78680ac80e1a
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-08-29 23:22:08 +0200

    Move config files to subdirectory

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -10,6 +10,7 @@ const runPerformanceTests = process.env.scope && ~process.env.scope.indexOf("per
 console.log(includeCoverage ? "coverage" : "", runUnitTests ? "unit" : "", runAcceptanceTests ? "acceptance" : "");
 
 const config = {
+	basePath: '../',
 	files: [
 		// {pattern: 'test/**/*_test.js', watched: false},
 		{
