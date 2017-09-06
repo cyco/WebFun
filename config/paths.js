7e56a6c7f98535bcceee502b789907b3c7a21936
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-09-06 23:10:43 +0200

    Refactor build system once more

---
+++ config/paths.js
@@ -0,0 +1,17 @@
+const Path = require('path');
+
+const projectRoot = Path.resolve(__dirname, "../");
+const sourceRoot = Path.resolve(projectRoot, "src");
+const testRoot = Path.resolve(projectRoot, "test");
+const toolsRoot = Path.resolve(projectRoot, "tools");
+const buildRoot = Path.resolve(projectRoot, "build");
+const configRoot = Path.resolve(projectRoot, "config");
+
+module.exports = {
+	projectRoot,
+	sourceRoot,
+	testRoot,
+	toolsRoot,
+	buildRoot,
+	configRoot
+};
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,76 +1,16 @@
 const Path = require("path");
+const Paths = require("./paths");
 
-const projectRoot = Path.resolve(__dirname, '../');
-const sourceRoot = Path.resolve(projectRoot, "src");
-
-module.exports = {
-	entry: "./src/index.js",
-	output: {
-		filename: "webfun.js",
-		path: Path.resolve(projectRoot, "build")
-	},
-	resolve: {
-		extensions: [ ".js", ".ts" ],
-		alias: {
-			"src": sourceRoot,
-			"std": Path.resolve(sourceRoot, "std"),
-			"std.dom": Path.resolve(sourceRoot, "std.dom"),
-			"std.webgl": Path.resolve(sourceRoot, "std.webgl"),
-			"libs": Path.resolve(sourceRoot, "libs"),
-			"test-helpers": Path.resolve(projectRoot, "test/helpers"),
-			"fs": Path.resolve(projectRoot, "test/helpers/polyfill/fs.js"),
-			"path": Path.resolve(projectRoot, "test/helpers/polyfill/path.js"),
-			// disable zlib (imported from KaitaiStream)
-			"zlib": Path.resolve(sourceRoot, "util/empty.js")
-		},
-		unsafeCache: true
-	},
-	module: {
-		rules: [ {
-			/* JavaScript / Babel */
-			test: /\.js$/,
-			loader: "babel-loader",
-			exclude: [ "node_modules" ]
-		}, {
-			/* TypeScript */
-			test: /\.ts$/,
-			loader: "awesome-typescript-loader",
-			exclude: [ "node_modules" ],
-			options: { configFileName: "./config/tsconfig.json" }
-		}, {
-			/* Styles */
-			test: /\.scss$/,
-			use: [ {
-				loader: "style-loader"
-			}, {
-				loader: "css-loader"
-			}, {
-				loader: "sass-loader",
-				options: {
-					includePaths: [ "./src/_style", "./" ]
-				}
-			} ],
-			exclude: [ "node_modules" ]
-		}, {
-			/* Shader */
-			test: /\.glsl?$/,
-			loader: "webpack-glsl-loader"
-		}, {
-			/* Kaitai-Struct definitions */
-			test: /\.ksy$/,
-			loader: "kaitai-struct-loader"
-		}, {
-			/** fonts **/
-			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-			loader: "url-loader?limit=10000&mimetype=application/font-woff"
-		}, { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
-		]
-	},
-	cache: true,
-	devtool: "inline-source-map",
-	devServer: {
-		publicPath: "/",
-		contentBase: Path.resolve(projectRoot, "build"),
-		hot: false
-	}
+const BaseConfig = require("./webpack.base");
+BaseConfig.entry = Path.resolve(Paths.sourceRoot, "index.js");
+BaseConfig.output = {
+	filename: "webfun.js",
+	path: Paths.buildRoot
 };
+BaseConfig.devServer = {
+	publicPath: "/",
+	contentBase: Paths.buildRoot,
+	hot: false
+};
+
+module.exports = BaseConfig;
