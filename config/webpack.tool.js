2e1fff1ddb64783b0f33183f4ec50252d0bb2c16
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-09-06 23:10:43 +0200

    Refactor build system once more

--- config/webpack.tool.js
+++ config/webpack.tool.js
@@ -2,12 +2,13 @@ const Path = require("path");
 const Webpack = require("webpack");
 const NodeExternals = require("webpack-node-externals");
 
-const Paths = require('./paths');
-const BaseConfig = require('./webpack.base.js');
+const Paths = require("./paths");
+const BaseConfig = require("./webpack.node.js");
 
-BaseConfig.entry = Path.resolve(Paths.toolsRoot, 'generate-world.js');
+const toolName = process.env.TOOL_NAME;
+BaseConfig.entry = Path.resolve(Paths.toolsRoot, toolName + ".js");
 BaseConfig.output = {
-	filename: "generate-world.bin.js",
+	filename: toolName + ".bin.js",
 	path: Paths.toolsRoot
 };
 
