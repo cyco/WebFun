f710265f23b857213472a39d94638445b3ee1f62
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-09-07 23:14:26 +0200

    Remove editor again

--- src/editor/components/window.js
+++
@@ -1,8 +0,0 @@
-import { Window } from "src/ui/components";
-import "./window.scss";
-
-export default class extends Window {
-	static get TagName() {
-		return "wf-editor-window";
-	}
-}
--- src/editor/editors/zone/tools/index.js
+++
@@ -1,7 +0,0 @@
-import MoveTool from "./move-tool";
-import BrushTool from "./brush-tool";
-import PaintBucketTool from "./paint-bucket-tool";
-import RectangleTool from "./rectangle-tool";
-import TilePickerTool from "./tile-picker-tool";
-
-export { MoveTool, BrushTool, PaintBucketTool, RectangleTool, TilePickerTool };
