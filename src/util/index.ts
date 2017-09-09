c2465534ce22f4acaa3a1cff6a282865062d20b6
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-09-09 12:31:46 +0200

    Explicitly import JS files in util

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,28 +1,28 @@
-import BatchLoader from "./batch-loader";
-import constantly from "./constantly";
-import Direction from "./direction";
-import dispatch from "./dispatch";
-import EventTarget from "./event-target";
-import FileLoader from "./file-loader";
-import HorizontalPointRange from "./horizontal-point-range";
-import identity from "./identity";
-import KeyEvent from "./key-event";
-import Message from "./message";
-import OutputStream from "./output-stream";
-import InputStream from "./input-stream";
-import Size from "./size";
-import Rectangle from "./rectangle";
-import Point from "./point";
+import BatchLoader from "./batch-loader.js";
+import constantly from "./constantly.js";
+import Direction from "./direction.js";
+import dispatch from "./dispatch.js";
+import EventTarget from "./event-target.js";
+import FileLoader from "./file-loader.js";
+import HorizontalPointRange from "./horizontal-point-range.js";
+import identity from "./identity.js";
+import KeyEvent from "./key-event.js";
+import Message from "./message.js";
+import OutputStream from "./output-stream.js";
+import InputStream from "./input-stream.js";
+import Size from "./size.js";
+import Rectangle from "./rectangle.js";
+import Point from "./point.js";
 
-import QueryString from "./query-string";
-import Range from "./range";
-import Stream from "./stream";
-import VerticalPointRange from "./vertical-point-range";
-import { rand, randmod, srand } from "./random";
-import { rgb, rgba } from "./color";
-import add from "./add";
+import QueryString from "./query-string.js";
+import Range from "./range.js";
+import Stream from "./stream.js";
+import VerticalPointRange from "./vertical-point-range.js";
+import { rand, randmod, srand } from "./random.js";
+import { rgb, rgba } from "./color.js";
+import add from "./add.js";
 
-import persistent from "./persistent";
+import persistent from "./persistent.js";
 
 export {
 	persistent,
