0c17a54758b0f20fa77d42e974e9715ff229e154
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-19 21:32:19 +0200

    Reformat page-objects

--- tools/page-objects/ammo-view.ts
+++ tools/page-objects/ammo-view.ts
@@ -1,8 +1,9 @@
 import PageObject from "./page-object";
-import MainWindow from "./main-window";
 
 class AmmoView extends PageObject {
-	public get selector(){ return 'wf-ammo-view'; }
+	public get selector() {
+		return "wf-ammo-view";
+	}
 }
 
 export default AmmoView;
