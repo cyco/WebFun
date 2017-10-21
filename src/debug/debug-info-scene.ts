029ab7f70f165ddefe616510ad4ca3b2a58b0332
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-21 17:35:01 +0200

    Stats overlay also shows ticks per second

--- src/debug/debug-info-scene.ts
+++ src/debug/debug-info-scene.ts
@@ -6,28 +6,40 @@ import { Point } from "../util";
 class DebugInfoScene extends Scene {
 	private _ticks = 0;
 	private _fps = 0;
+	private _tps = 0;
 	private _lastFrame = performance.now();
+	private _lastTick = performance.now();
 
 	public render(renderer: Renderer): void {
 		if (!(renderer instanceof CanvasRenderer)) return;
 
-		this.calculateFPS();
+		this.recalculateFPS();
 
-		renderer.renderText(`${this._fps.toFixed(0)} fps`, new Point(10, 246));
-		renderer.renderText(`${this._ticks} ticks`, new Point(10, 265));
+		renderer.renderText(`${this._ticks} ticks`, new Point(10, 246));
+		renderer.renderText(`${this._fps.toFixed(0)} fps, ${this._tps.toFixed(0)} tps`, new Point(10, 265));
 	}
 
 	public update(ticks: number): void {
 		this._ticks += ticks;
+		this.recalculateTPS();
 	}
 
-	private calculateFPS() {
-		const smoothing = 0.9;
+	private recalculateTPS() {
 		const now = performance.now();
-		const secondsSinceLastFrame = (now - this._lastFrame) / 1000;
+		const currentTPS = 1000 / (now - this._lastTick);
+		this._tps = this.smoothen(currentTPS, this._tps, 0.1);
+		this._lastTick = now;
+	}
 
-		this._fps = (this._fps * smoothing) + (1 / secondsSinceLastFrame * (1.0 - smoothing));
+	private recalculateFPS() {
+		const now = performance.now();
+		const currentFPS = 1000 / (now - this._lastFrame);
+		this._fps = this.smoothen(currentFPS, this._fps);
 		this._lastFrame = now;
+	}
+
+	private smoothen(value: number, lastValue: number, weight = 0.1) {
+		return value * weight + (lastValue * (1 - weight));
 	}
 }
 
