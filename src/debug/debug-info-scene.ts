1700d5a96d8c74ec4b864d999d51ce16aea9d214
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-21 12:32:21 +0200

    Debug info shows fps.

--- src/debug/debug-info-scene.ts
+++ src/debug/debug-info-scene.ts
@@ -5,12 +5,25 @@ import { Point } from "../util";
 
 class DebugInfoScene extends Scene {
 	private _ticks = 0;
-	private readonly _ticksLocation = new Point(10, 10);
+	private _fps = 0;
+	private _lastFrame = performance.now();
 
 	public render(renderer: Renderer): void {
 		if (!(renderer instanceof CanvasRenderer)) return;
 
-		renderer.renderText(`Ticks: ${this._ticks}`, this._ticksLocation);
+		this.calculateFPS();
+
+		renderer.renderText(`${this._fps.toFixed(0)} fps`, new Point(10, 246));
+		renderer.renderText(`${this._ticks} ticks`, new Point(10, 265));
+	}
+
+	private calculateFPS() {
+		const smoothing = 0.9;
+		const now = performance.now();
+		const secondsSinceLastFrame = (now - this._lastFrame) / 1000;
+
+		this._fps = (this._fps * smoothing) + (1 / secondsSinceLastFrame * (1.0 - smoothing));
+		this._lastFrame = now;
 	}
 
 	public update(ticks: number): void {
