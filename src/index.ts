d5f50e81a56a426ed9ef14cce6af2702c3ac2a33
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-21 17:24:35 +0200

    Remove unused imports from main entry point

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import "babel-polyfill";
-
 import "./_style/global.scss";
+
 import { main } from "./app";
-import "./debug";
 import "./extension";
-import "./ui";
-import "./util";
 
 window.addEventListener("load", main);
