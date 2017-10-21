8c00ddbf43e06c246f875fe680bdb4f0f55def48
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-21 17:31:25 +0200

    Make sure extensions are set up before any other code is executed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "babel-polyfill";
-import "./_style/global.scss";
-
-import { main } from "./app";
 import "./extension";
+
+import "./_style/global.scss";
+import { main } from "./app";
 
 window.addEventListener("load", main);
