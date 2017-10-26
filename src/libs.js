e6670d92c646ea28ab8d15e2ce949984eae4a15d
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-26 19:51:12 +0200

    Update import for new twgl version

--- src/libs.js
+++ src/libs.js
@@ -1,5 +1,5 @@
 import { KaitaiStream } from "kaitai-struct";
 import { sprintf } from "sprintf-js";
-import twgl from "twgl.js/dist/3.x/twgl.js";
+import twgl from "twgl.js/dist/4.x/twgl.js";
 
 export { twgl, KaitaiStream, sprintf };
