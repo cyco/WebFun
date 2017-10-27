26fc00293f0bbd357f821c00c0c6eb9c5f7b91ad
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-27 20:23:35 +0200

    Reformat code

--- src/app/main.ts
+++ src/app/main.ts
@@ -15,7 +15,7 @@ export default () => {
 	const gameController = new GameController();
 	gameController.start();
 
-	if(Settings.debuggerActive) {
+	if (Settings.debuggerActive) {
 		ScriptDebugger.sharedDebugger.show();
 	}
 };
