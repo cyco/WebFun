d5469e27fe476e0d91ea1e16f99564f5f5f554a8
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-28 12:17:25 +0200

    Register editor components on startup

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -1,7 +1,9 @@
 import Menu from "./menu";
+import { ComponentRegistry } from "src/ui";
+import * as Components from "./components";
 
 const initialize = () => {
-
+	ComponentRegistry.sharedRegistry.registerComponents(<any>Components);
 };
 
 export { initialize, Menu };
