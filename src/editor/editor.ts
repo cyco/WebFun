9a792152244855c8782baf940a1508ba46b55162
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-28 13:18:45 +0200

    Propagade storage to all inspectors so they can used it to store/restore their state between runs

---
+++ src/editor/editor.ts
@@ -0,0 +1,31 @@
+import DataManager from "./data-manager";
+import AbstractInspector from "src/editor/inspectors/abstract-inspector";
+import PrefixedStorage from "src/util/prefixed-storage";
+
+class Editor {
+	private static _sharedEditor: Editor;
+	public static set sharedEditor(e: Editor) {
+		this._sharedEditor = e;
+	}
+
+	public static get sharedEditor() {
+		return this._sharedEditor;
+	}
+
+	private _stateStorage: Storage;
+	private _inspectors: {[_: string]: AbstractInspector};
+
+	constructor(inspectors: {[_: string]: AbstractInspector}) {
+		this._inspectors = inspectors;
+	}
+
+	public set storage(storage: Storage) {
+		this._stateStorage = storage;
+
+		this._inspectors.each<AbstractInspector>((key: string, inspector: AbstractInspector): void => {
+			inspector.state = new PrefixedStorage(storage, key);
+		});
+	}
+}
+
+export default Editor;
--- src/editor/index.ts
+++ src/editor/index.ts
@@ -1,9 +1,29 @@
 import Menu from "./menu";
 import { ComponentRegistry } from "src/ui";
 import * as Components from "./components";
+import Editor from "src/editor/editor";
+import TileInspector from "src/editor/inspectors/tile-inspector";
+import ZoneInspector from "src/editor/inspectors/zone-inspector";
+import SoundInspector from "src/editor/inspectors/sound-inspector";
+import PuzzleInspector from "src/editor/inspectors/puzzle-inspector";
+import CharacterInspector from "src/editor/inspectors/character-inspector";
+import SetupImageInspector from "src/editor/inspectors/setup-image-inspector";
+import PrefixedStorage from "src/util/prefixed-storage";
 
 const initialize = () => {
 	ComponentRegistry.sharedRegistry.registerComponents(<any>Components);
+
+	const inspectors = {
+		"tile": new TileInspector(),
+		"zone": new ZoneInspector(),
+		"sound": new SoundInspector(),
+		"puzzle": new PuzzleInspector(),
+		"character": new CharacterInspector(),
+		"setup": new SetupImageInspector()
+	};
+
+	Editor.sharedEditor = new Editor(inspectors);
+	Editor.sharedEditor.storage = new PrefixedStorage(localStorage, "editor");
 };
 
 export { initialize, Menu };
