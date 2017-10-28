55d5980b8e4422620dc759a540014b7083f03d7d
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-10-28 13:35:51 +0200

    Hook up editor menu items to show corresponding inspectors

--- src/editor/inspectors/abstract-inspector.ts
+++ src/editor/inspectors/abstract-inspector.ts
@@ -1,5 +1,12 @@
+import { Window } from "src/ui/components";
+
 abstract class AbstractInspector {
 	public state: Storage;
+	public window: Window;
+
+	show() {
+		document.body.appendChild(this.window);
+	}
 }
 
 export default AbstractInspector;
