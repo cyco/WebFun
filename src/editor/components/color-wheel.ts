f0764d9c5545aa9320b85905d3f58f562d8a6351
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-11-04 14:40:16 +0100

    Add brightness option to color wheel

--- src/editor/components/color-wheel.ts
+++ src/editor/components/color-wheel.ts
@@ -1,6 +1,6 @@
-import { Component } from "src/ui";
+import {Component} from "src/ui";
 import "./color-wheel.scss";
-import { hsv2rgb, polar2xy, rad2deg, rgba, xy2polar } from "src/util";
+import {hsv2rgb, polar2xy, rad2deg, rgba, xy2polar} from "src/util";
 import Size from "src/util/size";
 
 class ColorWheel extends Component implements EventListenerObject {
@@ -11,17 +11,10 @@ class ColorWheel extends Component implements EventListenerObject {
 	private size = new Size(200, 200);
 	private radius = this.size.width / 2;
 	private crosshairColor = rgba(0, 0, 0, 0.9);
+	private _brightness: number = 1.0;
+	private _image: ImageData;
 
 	connectedCallback() {
-		const drawnSize = this.size.scaleBy(window.devicePixelRatio);
-
-		this.style.width = `${this.size.width}px`;
-		this.style.height = `${this.size.height}px`;
-		this._canvas.style.width = `${this.size.width}px`;
-		this._canvas.style.height = `${this.size.height}px`;
-		this._canvas.width = drawnSize.width;
-		this._canvas.height = drawnSize.height;
-
 		this.appendChild(this._canvas);
 		this.appendChild(this._crosshair);
 
@@ -30,10 +23,31 @@ class ColorWheel extends Component implements EventListenerObject {
 		this._crosshair.style.width = "15px";
 		this._crosshair.style.height = "15px";
 
-		this.drawWheel(drawnSize, this.radius * window.devicePixelRatio);
 		this.drawCrosshair();
+		this.update();
 
 		this.addEventListener("mousedown", this);
+	}
+
+	private update() {
+		const ctx = this._canvas.getContext("2d");
+		const drawnSize = this.size.scaleBy(window.devicePixelRatio);
+
+		if (!this._image) {
+			this.style.width = `${this.size.width}px`;
+			this.style.height = `${this.size.height}px`;
+			this._canvas.style.width = `${this.size.width}px`;
+			this._canvas.style.height = `${this.size.height}px`;
+			this._canvas.width = drawnSize.width;
+			this._canvas.height = drawnSize.height;
+
+			this._image = this.createColorWheelImage(ctx, drawnSize);
+		}
+
+		ctx.clearRect(0, 0, drawnSize.width, drawnSize.height);
+		ctx.putImageData(this._image, 0, 0);
+		ctx.fillStyle = rgba(0, 0, 0, 1 - this._brightness);
+		ctx.fillRect(0, 0, drawnSize.width, drawnSize.height);
 	}
 
 	public handleEvent(e: Event) {
@@ -69,12 +83,6 @@ class ColorWheel extends Component implements EventListenerObject {
 		this._crosshair.style.top = (y + this.size.height / 2 - 7.5) + "px";
 	}
 
-	private drawWheel(size: Size, radius = size.width / 2): void {
-		const ctx = this._canvas.getContext("2d");
-		const image = this.createColorWheelImage(ctx, size);
-		ctx.putImageData(image, 0, 0);
-	}
-
 	private createColorWheelImage(ctx: CanvasRenderingContext2D, size: Size) {
 		const radius = size.width / 2.0;
 		const width = size.width;
@@ -92,7 +100,7 @@ class ColorWheel extends Component implements EventListenerObject {
 				const cy = y - radius - centerY;
 
 				const [rho, theta] = xy2polar(cx, cy);
-				if (rho >= radius) continue;
+				if (rho > radius) continue;
 
 				const [r, g, b] = hsv2rgb(rad2deg(theta), rho / radius, 1);
 				const index = y * width + (width - x - 1);
@@ -122,6 +130,16 @@ class ColorWheel extends Component implements EventListenerObject {
 	disconnectedCallback() {
 		this._canvas.remove();
 		this.removeEventListener("mousedown", this);
+	}
+
+	set brightness(b: number) {
+		this._brightness = b;
+
+		this.update();
+	}
+
+	get brightness() {
+		return this._brightness;
 	}
 }
 
