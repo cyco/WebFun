51d86fb6dbfb34cc112b1bf0383d39c2d8656b67
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-11-04 18:04:15 +0100

    Flip y-axis in polar-coordinate calcuations

--- src/util/xy2polar.ts
+++ src/util/xy2polar.ts
@@ -1,6 +1,6 @@
 const xy2polar = (x: number, y: number): [number, number] => {
 	const rho = Math.sqrt(x * x + y * y);
-	const theta = Math.atan2(y, x);
+	const theta = Math.atan2(-y, x);
 	return [rho, theta];
 };
 
