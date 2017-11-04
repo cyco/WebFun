25fa107137ae1aa073f2d1d9c63d18386aede7c6
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-11-05 00:14:03 +0100

    Implement field editor to enable inline text editing in arbitary nodes.

--- src/ux/index.ts
+++ src/ux/index.ts
@@ -1,6 +1,15 @@
 import ModalSession from "./modal-session";
-import ModalConfirm, { Options as ConfirmationOptions, Result as ConfirmationResult } from "./modals/confirm";
+import ModalConfirm, {Options as ConfirmationOptions, Result as ConfirmationResult} from "./modals/confirm";
 import ResetCursor from "./reset-cursor";
 import WindowModalSession from "./window-modal-session";
+import FieldEditor from './field-editor';
 
-export { ModalSession, ResetCursor, WindowModalSession, ModalConfirm, ConfirmationResult, ConfirmationOptions };
+export {
+	FieldEditor,
+	ModalSession,
+	ResetCursor,
+	WindowModalSession,
+	ModalConfirm,
+	ConfirmationResult,
+	ConfirmationOptions
+};
