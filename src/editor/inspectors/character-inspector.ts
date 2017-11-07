a2e1d9023908f30c46fc6c10ad05e1fffe689788
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-11-07 21:25:45 +0100

    Char inspector lists all characters

--- src/editor/inspectors/character-inspector.ts
+++ src/editor/inspectors/character-inspector.ts
@@ -1,14 +1,41 @@
 import AbstractInspector from "src/editor/inspectors/abstract-inspector";
+import { List } from "src/ui/components";
+import { CharacterInspectorCell } from "../components";
+import Char from "src/engine/objects/char";
 
 class CharacterInspector extends AbstractInspector {
+	private _list: List<Char>;
+
 	constructor(state: Storage) {
 		super(state);
 
 		this.window.title = "Characters";
 		this.window.autosaveName = "character-inspector";
+		this.window.style.width = "230px";
+		this.window.content.style.height = "450px";
+		this.window.content.style.flexDirection = "column";
+
+		this._list = <List<Char>>document.createElement(List.TagName);
+		this._list.cell = <CharacterInspectorCell>document.createElement(CharacterInspectorCell.TagName);
+		this._list.classList.add("character-inspector-list");
+		this._list.searchDelegate = this;
+		this._list.state = state.prefixedWith("list");
+
+		this.window.content.appendChild(this._list);
 	}
 
-	build() {
+	public build() {
+		const cell = <CharacterInspectorCell>this._list.cell;
+		cell.tileSheet = this.data.tileSheet;
+		this._list.items = this.data.currentData.characters;
+	}
+
+	public prepareListSearch(searchValue: string, list: List<Char>): any {
+		return null;
+	}
+
+	public includeListItem(searchValue: any, item: Char, cell: CharacterInspectorCell, list: List<Char>): boolean {
+		return true;
 	}
 }
 
