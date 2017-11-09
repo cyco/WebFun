11ba9a2f80504699af849dfc3b70949205507429
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-11-09 21:43:30 +0100

    Add search bar to zone list

--- src/editor/inspectors/zone-inspector.ts
+++ src/editor/inspectors/zone-inspector.ts
@@ -18,6 +18,8 @@ class ZoneInspector extends AbstractInspector {
 
 		this._list = <List<Zone>>document.createElement(List.TagName);
 		this._list.cell = <ZoneInspectorCell>document.createElement(ZoneInspectorCell.TagName);
+		this._list.searchDelegate = this;
+		this._list.state = state.prefixedWith("list");
 
 		this._editor = <ZoneEditor>document.createElement(ZoneEditor.TagName);
 
@@ -29,6 +31,23 @@ class ZoneInspector extends AbstractInspector {
 		const cell = <ZoneInspectorCell>this._list.cell;
 		cell.tileSheet = this.data.tileSheet;
 		this._list.items = this.data.currentData.zones;
+	}
+
+	prepareListSearch(searchValue: string, list: List<Zone>): RegExp[] {
+		this.stateDidChange();
+		return searchValue.split(" ").map(s => new RegExp(s, "i"));
+	}
+
+	includeListItem(searchValue: RegExp[], item: Zone, cell: ZoneInspectorCell, list: List<Zone>): boolean {
+		const searchableAttributes = [
+			item.id,
+			item.size.width,
+			item.size.height,
+			item.type.name,
+			item.planet.name
+		];
+		const string = searchableAttributes.join(" ");
+		return searchValue.every(r => r.test(string));
 	}
 }
 
