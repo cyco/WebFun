53ddd415ef609a9a184b7c1ee82e117c68daaf4c
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-11-16 12:32:00 +0100

    Add Event class for use with es6. Original Event base class needs to be instantiated using reflection.

---
+++ src/util/event.ts
@@ -0,0 +1,16 @@
+function BaseEvent(type: string, init: EventInit) {
+	const newTarget = this.__proto__.constructor;
+	return Reflect.construct(Event, [type, init], newTarget);
+}
+
+Object.setPrototypeOf(BaseEvent, Event);
+Object.setPrototypeOf(BaseEvent.prototype, Event.prototype);
+
+
+declare interface BaseEvent extends Event {
+	prototype: Event;
+
+	new(): Event;
+}
+
+export default <typeof Event><any>BaseEvent;
