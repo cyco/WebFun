ea67530368575d524be44e67b8afdf33970b4652
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-11-21 20:22:29 +0100

    Show errors when action input can't be parsed on save.

--- src/editor/components/action-editor/editor.ts
+++ src/editor/components/action-editor/editor.ts
@@ -4,60 +4,108 @@ import { Action } from "src/engine/objects";
 import "./editor.scss";
 import Printer from "src/editor/components/action-editor/printer";
 import Disassembler from "src/editor/components/action-editor/disassembler";
-import Assembler from "src/editor/components/action-editor/assembler";
-import Parser from "src/editor/components/action-editor/parser";
+import Assembler, { AssemblerInputError } from "src/editor/components/action-editor/assembler";
+import Parser, { ParserError } from "src/editor/components/action-editor/parser";
+import { Shortcut } from "src/ux";
+import ShortcutManager from "src/ux/shortcut-manager";
 
 class Editor extends Component {
 	static readonly TagName = "wf-action-editor";
 	static readonly observedAttributes: string[] = [];
 	private _action: Action;
+	private _shortcuts: Shortcut[];
+	private _errorArea: HTMLDivElement;
+	private _editorArea: HTMLDivElement;
+
+	constructor() {
+		super();
+
+		this._errorArea = document.createElement("div");
+		this._errorArea.classList.add("error-area");
+		this._errorArea.style.display = "none";
+	}
 
 	connectedCallback() {
 		super.connectedCallback();
 		this.updateWindowTitle();
+		this.registerShortcuts();
 	}
 
-	disconnectedCallback() {
-		super.disconnectedCallback();
+	private registerShortcuts() {
+		const manager = ShortcutManager.sharedManager;
+		const shortcut = manager.registerShortcut(() => this.save(), {node: this, metaKey: true, keyCode: 83});
+
+		this._shortcuts = [shortcut];
 	}
 
-	set action(action: Action) {
-		this._action = action;
+	public save() {
+		this._errorArea.textContent = "";
 
-		const div = document.createElement("div");
-		div.spellcheck = false;
-
-		const printer = new Printer();
-		const disassembler = new Disassembler();
-		const assembler = new Assembler();
 		const parser = new Parser();
+		const assembler = new Assembler();
 
 		try {
-			let ast = disassembler.disassemble(action);
-			ast = disassembler.disassemble(assembler.assemble(ast));
+			const input = this._editorArea.textContent;
+			const ast = parser.parse(input);
+			if (ast.length !== 1)
+				throw new Error(ast.length ? "Too many defintions found!" : "Not enough defintions found!");
+			const action = assembler.assemble(ast.first());
 
-			div.innerHTML = printer.pprint(ast);
-			div.setAttribute("contenteditable", "");
-
-			try {
-				parser.parse(div.textContent);
-			} catch (e) {
-				div.innerHTML += `<br><span class="error">${e.message}</span><br>${printer.pprint(e.input)}`;
-			}
+			this._errorArea.style.display = "none";
+			this.style.setProperty("--error-height", "0px");
 		} catch (e) {
-			div.innerHTML = `<span class="error">${e.message}</span><br>${printer.pprint(e.input)}`;
+			if (e instanceof ParserError) {
+				this._errorArea.textContent = "SyntaxError: " + e.message;
+			} else if (e instanceof AssemblerInputError) {
+				this._errorArea.textContent = "Assembler Error: " + e.message;
+			} else {
+				this._errorArea.textContent = "Error: " + e.message;
+			}
+			this._errorArea.style.display = "";
+			this.style.setProperty("--error-height", this._errorArea.getBoundingClientRect().height + "px");
 		}
+	}
 
-		this.textContent = "";
-		this.appendChild(div);
-
-		this.updateWindowTitle();
+	private unregisterShortcuts() {
+		const manager = ShortcutManager.sharedManager;
+		this._shortcuts.forEach(sc => manager.unregisterShortcut(sc));
+		this._shortcuts = [];
 	}
 
 	private updateWindowTitle() {
 		if (!this._action) return;
 		const window = <Window>this.closest(Window.TagName);
 		if (window) window.title = `Zone ${this._action.zone.id}: Action ${this._action.id}`;
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback();
+		this.unregisterShortcuts();
+	}
+
+	set action(action: Action) {
+		this._action = action;
+
+		const div = document.createElement("div");
+		div.classList.add("editor-area");
+		div.spellcheck = false;
+
+		const printer = new Printer();
+		const disassembler = new Disassembler();
+
+		try {
+			let ast = disassembler.disassemble(action);
+			div.innerHTML = printer.pprint(ast);
+			div.setAttribute("contenteditable", "");
+		} catch (e) {
+			div.innerHTML = `<span class="error">${e.message}</span><br>${printer.pprint(e.input)}`;
+		}
+
+		this.textContent = "";
+		this._editorArea = div;
+		this.appendChild(this._errorArea);
+		this.appendChild(div);
+		this.updateWindowTitle();
 	}
 
 	get action() {
