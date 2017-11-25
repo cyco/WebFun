4d890d97e6f413c6d774092e6cc2d51751274ace
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-11-25 21:24:40 +0100

    Re-enable proper argument count in disassembler and adjust some instructions.

--- src/engine/script/instructions/play-sound.ts
+++ src/engine/script/instructions/play-sound.ts
@@ -4,7 +4,7 @@ import Instruction from "../../objects/instruction";
 import { Result, ResultFlags } from "../arguments";
 
 export const Opcode = 0x0a;
-export const Arguments = -1;
+export const Arguments = 1;
 export const Description = "Play sound specified by `arg_0`";
 export default (instruction: Instruction, engine: Engine, action: Action): Result => {
 	const args = instruction.arguments;
--- src/engine/script/instructions/set-tile-needs-display.ts
+++ src/engine/script/instructions/set-tile-needs-display.ts
@@ -4,7 +4,7 @@ import Instruction from "../../objects/instruction";
 import { Result, ResultFlags } from "../arguments";
 
 export const Opcode = 0x06;
-export const Arguments = -1;
+export const Arguments = 2;
 export const Description = "Redraw tile at `arg_0`x`arg_1`";
 export default (instruction: Instruction, engine: Engine, action: Action): Result => ResultFlags.OK;
 /*
