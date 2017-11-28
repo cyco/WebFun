dc611b4686cf8a185972e09d98d40f558f3fafcb
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-11-28 20:25:47 +0100

    Fix condition arguments.

--- src/engine/script/conditions/enter-by-plane.ts
+++ src/engine/script/conditions/enter-by-plane.ts
@@ -4,8 +4,7 @@ import { int16 } from "../types";
 import Condition from "src/engine/script/condition";
 
 export default <Condition>{
-
 	Opcode: 0x09,
-	Arguments: 0,
+	Arguments: [],
 	Implementation: async (args: int16[], zone: Zone, engine: Engine): Promise<boolean> => engine.state.enteredByPlane
 };
