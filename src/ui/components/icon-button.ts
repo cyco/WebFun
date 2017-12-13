6787203403ac3c07b35827d731b9dd3d1ee6b97c
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-12-13 22:06:34 +0100

    Use icon-button instead of creating explicit i.fa-elements

--- src/ui/components/icon-button.ts
+++ src/ui/components/icon-button.ts
@@ -3,42 +3,37 @@ import "./icon-button.scss";
 
 class IconButton extends Component {
 	public static TagName = "wf-icon-button";
-	public static Options = {extends: "button"};
+	public static Options = { extends: "button" };
 	public static observedAttributes = ["icon"];
-
 	private _icon: HTMLElement;
+	private _iconName: string;
 
-	constructor(iconName = "") {
+	constructor() {
 		super();
 
-		const icon = document.createElement("i");
-		icon.classList.add("fa");
-		this._icon = icon;
-		this.onclick = null;
-
-		if (iconName) this.setAttribute("icon", iconName);
+		this._icon = document.createElement("i");
 	}
 
 	get icon() {
-		return this.getAttribute("icon");
+		return this._iconName;
 	}
 
-	set icon(i) {
-		this.setAttribute("icon", i);
+	set icon(i: string) {
+		this._iconName = i;
+		this._icon.className = `fa fa-${i}`;
 	}
 
 	get disabled() {
 		return this.hasAttribute("disabled");
 	}
 
-	set disabled(d) {
+	set disabled(d: boolean) {
 		if (d) this.setAttribute("disabled", "");
 		else this.removeAttribute("disabled");
 	}
 
 	connectedCallback() {
-		this._icon.remove();
-		this.appendChild(this._icon);
+		if (!this._icon.parentNode) this.appendChild(this._icon);
 	}
 
 	attributeChangedCallback(attributeName: string, oldValue: string, newValue: string): void {
