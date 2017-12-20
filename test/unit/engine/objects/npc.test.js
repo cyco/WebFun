82d85874b3cbcbee4dda230a0c59ac462b0c94e8
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-12-20 14:30:48 +0100

    Fix test execution.

--- test/unit/engine/objects/npc.test.js
+++ test/unit/engine/objects/npc.test.js
@@ -2,19 +2,7 @@ import NPC from "src/engine/objects/npc";
 
 describe("NPC", () => {
 	it("is a class representing an npc", () => {
-		const intValues = {
-			face: null,
-			x: 4,
-			y: 3,
-			unknown1: null,
-			unknown2: null,
-			unknown3: null
-		};
-
-		const npc = new NPC(intValues);
-
-		expect(npc.position.x).toBe(4);
-		expect(npc.position.y).toBe(3);
+		const npc = new NPC();
 		expect(npc.enabled).toBeTrue();
 	});
 });
