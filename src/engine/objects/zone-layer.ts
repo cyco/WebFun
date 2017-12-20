a410321f9edebb21f9ab0d49a359b01d2db0b4fd
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2017-12-20 14:31:03 +0100

    'No tool' shows info for the tiles below the cursor.

--- src/engine/objects/zone-layer.ts
+++ src/engine/objects/zone-layer.ts
@@ -1,5 +1,13 @@
 export default {
 	Floor: 0,
 	Object: 1,
-	Roof: 2
+	Roof: 2,
+
+	NameFromNumber(layer: number) {
+		switch (layer) {
+			case 0: return 'floor';
+			case 1: return 'object';
+			case 2: return 'roof';
+		}
+	}
 };
