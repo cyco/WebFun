4ba0f50a8387d56d4155c0cd02147af4ef630573
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-01-30 21:55:55 +0100

    Fix test for speak-npc instruction.

--- test/unit/ui/components/speech-bubble.test.js
+++ test/unit/ui/components/speech-bubble.test.js
@@ -2,7 +2,7 @@ import SpeechBubble from "src/ui/components/speech-bubble";
 
 describeComponent(SpeechBubble, () => {
 	let subject;
-	beforeEach(() => (subject = render(SpeechBubble)));
+	beforeEach(() => subject = render(SpeechBubble));
 
 	it("is a class that displays a bubble with text", () => {
 		expect(SpeechBubble).toBeClass();
