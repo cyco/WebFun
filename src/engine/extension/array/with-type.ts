4b9f667e46ed8c3024d4a2340e40ad5139a73849
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-02-01 20:51:25 +0100

    Declare extended array interface for withType-extension.

--- src/engine/extension/array/with-type.ts
+++ src/engine/extension/array/with-type.ts
@@ -1,6 +1,13 @@
-const withType = function<T>(searchType: T) {
+const withType = function <T>(searchType: T) {
 	return this.filter(({ type }: { type: T }) => type === searchType);
 };
+
 Array.prototype.withType = withType;
+
+declare global {
+	interface Array<T> {
+		withType<E>(searchType: E): Array<T>
+	}
+}
 
 export default withType;
