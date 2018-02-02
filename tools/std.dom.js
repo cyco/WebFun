30160915918785a35b688d0e78e8ba51dcb67bbe
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-02-02 21:52:14 +0100

    Mock dom classes so all extensions can be loaded in node tools.

---
+++ tools/std.dom.js
@@ -0,0 +1,10 @@
+import {global} from 'std';
+
+global.Storage = function(){};
+global.Event = function(){};
+global.Element = function(){};
+global.HTMLCollection = function(){};
+global.Image = function(){};
+global.MouseEvent = function(){};
+global.NodeList = function(){};
+global.File = function(){};
