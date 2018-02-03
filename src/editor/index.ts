6aec5e6e4ec39f333c32f3e24104af4d16659691
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-02-03 12:33:24 +0100

    Export CSSTileSheet from editor.

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -2,10 +2,11 @@ import { ComponentRegistry } from "src/ui";
 import * as Components from "./components";
 import Editor from "./editor";
 import GameController from "src/app/game-controller";
+import CSSTileSheet from "./css-tile-sheet";
 
 const initialize = (gameController: GameController) => {
 	ComponentRegistry.sharedRegistry.registerComponents(<any>Components);
 	ComponentRegistry.sharedRegistry.registerComponent(Editor);
 };
 
-export { initialize };
+export { initialize, CSSTileSheet };
