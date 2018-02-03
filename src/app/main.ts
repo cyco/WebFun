4f54b7e6cf23fbd6ab3d9c349331382483a0aa71
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-02-03 18:37:59 +0100

    Setup global jsx factory on load.

--- src/app/main.ts
+++ src/app/main.ts
@@ -6,8 +6,16 @@ import Settings, { loadSettings } from "src/settings";
 import { initialize as initializeDebug } from "src/debug";
 import { initialize as initializeEditor } from "src/editor";
 import { initialize as initializeSaveGameEditor } from 'src/save-game-editor';
+import { ComponentJSXRenderer } from 'src/ui';
+
+declare global {
+	interface Window {
+		WebFunJSX: ComponentJSXRenderer;
+	}
+}
 
 export default () => {
+	window.WebFunJSX = new ComponentJSXRenderer();
 	loadSettings();
 
 	ComponentRegistry.sharedRegistry.registerComponents(<any>Components);
