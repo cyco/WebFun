f6d4ac28336df84bbe0ee6a6f80a475fa8c616cb
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-02-04 16:28:15 +0100

    Run prettier again.

--- src/save-game-editor/components/interactive-health.ts
+++ src/save-game-editor/components/interactive-health.ts
@@ -6,58 +6,58 @@ import { MouseEvent } from "src/std.dom";
 const FlipThreshold = 180;
 
 class InteractiveHealth extends Health implements EventListenerObject {
-  public static readonly TagName = "wf-save-game-editor-health";
-  private lastAngle: number = null;
+	public static readonly TagName = "wf-save-game-editor-health";
+	private lastAngle: number = null;
 
-  connectedCallback() {
-    super.connectedCallback();
-    this.addEventListener("mousedown", this);
-  }
+	connectedCallback() {
+		super.connectedCallback();
+		this.addEventListener("mousedown", this);
+	}
 
-  handleEvent(event: MouseEvent) {
-    if (event.type === "mousedown") {
-      document.addEventListener("mouseup", this, { capture: true } as any);
-      document.addEventListener("mousemove", this, { capture: true } as any);
-    }
+	handleEvent(event: MouseEvent) {
+		if (event.type === "mousedown") {
+			document.addEventListener("mouseup", this, { capture: true } as any);
+			document.addEventListener("mousemove", this, { capture: true } as any);
+		}
 
-    if (event.type === "mouseup") {
-      document.removeEventListener("mouseup", this, { capture: true } as any);
-      document.removeEventListener("mousemove", this, { capture: true } as any);
-    }
+		if (event.type === "mouseup") {
+			document.removeEventListener("mouseup", this, { capture: true } as any);
+			document.removeEventListener("mousemove", this, { capture: true } as any);
+		}
 
-    const { left, width, top, height } = this.getBoundingClientRect();
-    const center = new Point(left + width / 2, top + height / 2);
-    const point = new Point(event.clientX, event.clientY).subtract(center);
-    const [distance, angle] = xy2polar(point.x, point.y);
+		const { left, width, top, height } = this.getBoundingClientRect();
+		const center = new Point(left + width / 2, top + height / 2);
+		const point = new Point(event.clientX, event.clientY).subtract(center);
+		const [distance, angle] = xy2polar(point.x, point.y);
 
-    let normangle = rad2deg(360 - angle) % 360;
-    let base = ceil(this.health / 100) * 100;
-    if (this.lastAngle === null) {
-      this.health = base - normangle / 360 * 100;
-      this.lastAngle = normangle;
-      return;
-    }
+		let normangle = rad2deg(360 - angle) % 360;
+		let base = ceil(this.health / 100) * 100;
+		if (this.lastAngle === null) {
+			this.health = base - normangle / 360 * 100;
+			this.lastAngle = normangle;
+			return;
+		}
 
-    if (this.lastAngle === normangle) {
-      return;
-    }
+		if (this.lastAngle === normangle) {
+			return;
+		}
 
-    const diff = normangle - this.lastAngle;
-    if (abs(diff) > FlipThreshold) {
-      base += sign(diff) * 100;
-    }
+		const diff = normangle - this.lastAngle;
+		if (abs(diff) > FlipThreshold) {
+			base += sign(diff) * 100;
+		}
 
-    this.health = base - normangle / 360 * 100;
-    this.lastAngle = normangle;
-  }
+		this.health = base - normangle / 360 * 100;
+		this.lastAngle = normangle;
+	}
 
-  disconnectedCallback() {
-    super.disconnectedCallback();
+	disconnectedCallback() {
+		super.disconnectedCallback();
 
-    this.removeEventListener("mousedown", this);
-    document.removeEventListener("mouseup", this, { capture: true } as any);
-    document.removeEventListener("mousemove", this, { capture: true } as any);
-  }
+		this.removeEventListener("mousedown", this);
+		document.removeEventListener("mouseup", this, { capture: true } as any);
+		document.removeEventListener("mousemove", this, { capture: true } as any);
+	}
 }
 
 export default InteractiveHealth;
