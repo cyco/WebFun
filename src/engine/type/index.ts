ae9597be9744a57fecff2024c636c8a502090178
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-04-25 23:39:50 +0200

    Split game types into classes.

---
+++ src/engine/type/index.ts
@@ -0,0 +1,7 @@
+import GameType from "./type";
+import IndyGameType from "./indy";
+import YodaGameType from "./yoda";
+
+export const Indy = new IndyGameType();
+export const Yoda = new YodaGameType();
+export { GameType };
---
+++ src/engine/type/yoda.ts
@@ -0,0 +1,9 @@
+import GameType from "./type";
+
+class Yoda extends GameType {
+	get saveGameMagic() {
+		return "YODASAV44";
+	}
+}
+
+export default Yoda;
