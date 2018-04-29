a398885016d5140f5236e6e914af3b46b12bfca0
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-04-29 10:16:26 +0200

    Strip string terminator from sound names.

--- src/engine/file-format/manual/sound.ts
+++ src/engine/file-format/manual/sound.ts
@@ -1,4 +1,5 @@
 import { InputStream } from "src/util";
+import { assert } from "../error";
 
 export const parseSounds = (stream: InputStream, data: any) => {
 	let size = stream.getUint32();
@@ -6,8 +7,10 @@ export const parseSounds = (stream: InputStream, data: any) => {
 	let sounds = new Array(count);
 	for (let i = 0; i < count; i++) {
 		let size = stream.getUint16();
-		let name = stream.getCharacters(size);
-		// ISO_8859_1.decode(&buffer, DecoderTrap::Strict);
+		assert(size > 0, `Expected at least as null byte to terminate an empty string!`, stream);
+		const name = stream.getCharacters(size - 1);
+		const stringTerminator = stream.getUint8();
+		assert(stringTerminator === 0, `Expected string to be null terminated!`, stream);
 		sounds[i] = name;
 	}
 	data.sounds = sounds;
