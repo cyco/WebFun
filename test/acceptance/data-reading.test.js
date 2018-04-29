2ca6a82949a27002779e90fca067c5d043a8a74c
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-04-29 12:30:33 +0200

    Add basic test for reading game data in various formats.

---
+++ test/acceptance/data-reading.test.js
@@ -0,0 +1,32 @@
+import { getFixtureData } from "test-helpers/fixture-loading";
+import { ManualDataFileReader, GameTypeYoda, GameTypeIndy } from "src/engine";
+import { InputStream } from "src/util";
+
+describe("DataReading", () => {
+	const loadData = async file => {
+		return new Promise((resolve, reject) => {
+			getFixtureData(file, result => {
+				resolve(result && result.byteLength > 9 ? new InputStream(result) : null);
+			});
+		});
+	};
+
+	const parsesWithoutError = (type, file) => async done => {
+		const data = await loadData(file);
+		if (!data) {
+			console.warn(
+				`Unable to find fixture data. Place ${file} into test/fixtures to run this test.`
+			);
+			done();
+			return;
+		}
+
+		expect(() => ManualDataFileReader(data, type)).not.toThrow();
+		done();
+	};
+
+	it("reads indy's data without errors", parsesWithoutError(GameTypeIndy, "indy.data"));
+	it("reads indy's demo data without errors", parsesWithoutError(GameTypeIndy, "indy-demo.data"));
+	it("reads yoda's data without errors", parsesWithoutError(GameTypeYoda, "yoda.data"));
+	it("reads yoda's demo data without errors", parsesWithoutError(GameTypeYoda, "yoda-demo.data"));
+});
