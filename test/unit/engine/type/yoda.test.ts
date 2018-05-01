4727543cfc44bbeca420cf89cde973e3e180dd46
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-05-01 09:07:39 +0200

    Add basic test for game types.

---
+++ test/unit/engine/type/yoda.test.ts
@@ -0,0 +1,18 @@
+import Yoda from "src/engine/type/yoda";
+import GameType from "src/engine/type/type";
+
+describe("WebFun.Engine.Type.Yoda", () => {
+	let subject: Yoda;
+	beforeEach(() => (subject = new Yoda()));
+	it("is a class representing a variation of the engine", () => {
+		expect(subject).toBeInstanceOf(GameType);
+	});
+
+	it("is used to identify a save game's format", () => {
+		expect(subject.saveGameMagic).toBe("YODASAV44");
+	});
+
+	it("is used to determine which tiles to show in map view", () => {
+		expect(subject.locatorTile.here).toEqual(639);
+	});
+});
