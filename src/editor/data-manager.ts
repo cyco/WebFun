d940684f2c9016b164e110c82aebc02af14540e9
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-05-13 15:59:35 +0200

    Show progres indicator while loading data for resource editor.

--- src/editor/data-manager.ts
+++ src/editor/data-manager.ts
@@ -3,26 +3,16 @@ import CSSTileSheet from "./css-tile-sheet";
 import { ImageFactory } from "src/engine/rendering/canvas";
 
 class DataManager {
-	private _originalData: GameData;
+	private _data: GameData;
 	private _currentData: GameData;
 	private _palette: ColorPalette;
 	private _tileSheet: CSSTileSheet;
 
-	constructor(originalData: GameData, palette: ColorPalette) {
-		this._originalData = originalData.copy();
-		this._currentData = originalData.copy();
+	constructor(data: GameData, palette: ColorPalette, tilesheet: CSSTileSheet) {
+		this._data = data.copy();
+		this._currentData = data.copy();
 		this._palette = palette;
-		this._tileSheet = new CSSTileSheet(this._originalData.tiles.length);
-		this._originalData.tiles.forEach(t => this._tileSheet.add(t.imageData));
-		this._tileSheet.draw(new ImageFactory(palette));
-	}
-
-	write(data: GameData, target: ArrayBuffer): number {
-		return 0;
-	}
-
-	get originalData() {
-		return this._originalData;
+		this._tileSheet = tilesheet;
 	}
 
 	get currentData() {
