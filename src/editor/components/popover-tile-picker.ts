fb714eb47d20a4796cbd0ec2aadd23822ee26945
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-05-13 21:18:40 +0200

    Tile picker can shows current tile.

--- src/editor/components/popover-tile-picker.ts
+++ src/editor/components/popover-tile-picker.ts
@@ -2,7 +2,8 @@ import { Component } from "src/ui";
 import { Tile } from "src/engine/objects";
 import DataManager from "src/editor/data-manager";
 import TileFilter from "src/editor/components/tile-filter";
-import "./tile-picker.scss";
+import { Tile as TileComponent } from "src/save-game-editor/components";
+import "./popover-tile-picker.scss";
 
 export const Events = {
 	TileDidChange: "TileDidChange"
@@ -14,29 +15,26 @@ class PopoverTilePicker extends Component {
 	private _tiles: Tile[];
 	private _data: DataManager;
 	private _currentTile: Tile;
+	private _currentTileView: TileComponent;
 
 	constructor() {
 		super();
+		this._currentTileView = document.createElement(TileComponent.TagName) as TileComponent;
 	}
 
 	connectedCallback() {
 		super.connectedCallback();
+		this.appendChild(this._currentTileView);
 	}
 
 	disconnectedCallback() {
 		super.disconnectedCallback();
 	}
 
-	set tiles(s) {
-		this._tiles = s;
-	}
-
-	get tiles() {
-		return this._tiles;
-	}
-
 	set data(d) {
 		this._data = d;
+		this._currentTileView.tileSheet = d.tileSheet;
+		this.currentTile = d.currentData.tiles[0x12];
 	}
 
 	get data() {
@@ -45,6 +43,7 @@ class PopoverTilePicker extends Component {
 
 	set currentTile(tile: Tile) {
 		this._currentTile = tile;
+		this._currentTileView.tile = tile;
 		this.dispatchEvent(
 			new CustomEvent(Events.TileDidChange, { detail: { tile }, bubbles: true })
 		);
