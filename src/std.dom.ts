190f312c95225e410944ffaacda72c1784a49477
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-08-01 21:47:59 +0200

    Use destructuring for re-exports.

--- src/std.dom.ts
+++ src/std.dom.ts
@@ -2,21 +2,23 @@ import { global } from "./std";
 
 const window = global.window;
 
-export const Storage = global.Storage;
-export const XMLHttpRequest = global.XMLHttpRequest;
-export const Element = global.Element;
-export const Image = global.Image;
-export const NodeList = global.NodeList;
-export const Node = global.Node;
-export const ImageData = global.ImageData;
-export const KeyEvent: { [_: string]: number } = global.KeyEvent || {};
-export const HTMLElement = global.HTMLElement;
-export const HTMLCollection = global.HTMLCollection;
-export const Event = global.Event;
-export const File = global.File;
-export const FileReader = global.FileReader;
-export const MouseEvent = global.MouseEvent;
+export const {
+	Storage,
+	XMLHttpRequest,
+	Element,
+	Image,
+	NodeList,
+	Node,
+	ImageData,
+	HTMLElement,
+	HTMLCollection,
+	Event,
+	File,
+	FileReader,
+	MouseEvent
+} = global;
 
 export { window };
+export const KeyEvent: { [_: string]: number } = global.KeyEvent || {};
 export const document = <Document>window.document;
 export const localStorage = <Storage>window.localStorage;
--- src/std.ts
+++ src/std.ts
@@ -8,15 +8,17 @@ if (!global.window) {
 	global.window = global;
 }
 
-export const setTimeout = global.setTimeout;
-export const console = global.console;
-export const Array = global.Array;
-export const ArrayBuffer = global.ArrayBuffer;
-export const Promise = global.Promise;
-export const DataView = global.DataView;
-export const Uint8Array = global.Uint8Array;
-export const requestAnimationFrame = global.requestAnimationFrame;
-export const cancelAnimationFrame = global.cancelAnimationFrame;
-export const performance = global.performance;
-export const Error = global.Error;
-export const TextDecoder = global.TextDecoder;
+export const {
+	setTimeout,
+	console,
+	Array,
+	ArrayBuffer,
+	Promise,
+	DataView,
+	Uint8Array,
+	requestAnimationFrame,
+	cancelAnimationFrame,
+	performance,
+	Error,
+	TextDecoder
+} = global;
