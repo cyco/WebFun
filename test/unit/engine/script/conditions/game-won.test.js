e6936586e8036bf2a4a804a48ff49969c35ff9ea
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-09-05 20:59:23 +0200

    Rename unknown condition to game-won. Thanks to @shinyquagsire23 for figuring this one out! https://github.com/shinyquagsire23/DesktopAdventures/blob/1b419d5f2615255ad06a3f6acd5e81ea744ca44f/scrdoc.txt#L21

---
+++ test/unit/engine/script/conditions/game-won.test.js
@@ -0,0 +1,4 @@
+import { Condition } from "src/engine/objects";
+import Unknown1Again from "src/engine/script/conditions/game-won";
+
+describeCondition("GameWon", (check, engine) => {});
--- test/unit/engine/script/conditions/unknown1-again.test.js
+++
@@ -1,4 +0,0 @@
-import { Condition } from "src/engine/objects";
-import Unknown1Again from "src/engine/script/conditions/unknown1-again";
-
-describeCondition("Unknown1Again", (check, engine) => {});
