3cf59299a32aa3cea6a963a12434ecbc3624224e
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-10-14 16:19:18 +0200

    Declare document.createElement overload for project-defined elements.

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -19,6 +19,19 @@ declare global {
 		WebFunJSX: ComponentJSXRenderer;
 	}
 }
+type Components<Entry> = Extract<Entry[keyof Entry], { readonly tagName: string } & { new (): any }>;
+type ComponentDefinitions<Bundle> = {
+	[TagName in Components<Bundle>["tagName"]]: Extract<Components<Bundle>, { tagName: TagName }>
+};
+type CustomElements = ComponentDefinitions<typeof Components> &
+	ComponentDefinitions<typeof AppComponents> &
+	ComponentDefinitions<typeof WindowComponents>;
+
+declare global {
+	interface Document {
+		createElement<T extends keyof CustomElements>(tagName: T): InstanceType<CustomElements[T]>;
+	}
+}
 
 let editorWindow: EditorWindow;
 const main = async () => {
