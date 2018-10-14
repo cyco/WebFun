5fac126ab0a3b98029e8ba707c42e3cf0ff00df3
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-10-14 16:20:56 +0200

    Move JSX type declarations so the real Component class is used.

--- src/ui/component.ts
+++ src/ui/component.ts
@@ -1,5 +1,5 @@
 abstract class Component extends HTMLElement {
-	public static tagName: string;
+	public static readonly tagName: string;
 	public static observedAttributes: string[];
 
 	protected adoptedCallback(): void {}
@@ -9,6 +9,36 @@ abstract class Component extends HTMLElement {
 	protected disconnectedCallback(): void {}
 
 	protected attributeChangedCallback(attributeName: string, oldValue: string, newValue: string): void {}
+}
+
+declare global {
+	namespace JSX {
+		export interface IntrinsicElements {
+			div: Partial<HTMLDivElement>;
+			label: Partial<HTMLLabelElement>;
+			span: Partial<HTMLSpanElement>;
+			input: Partial<HTMLInputElement>;
+			canvas: Partial<HTMLCanvasElement>;
+			img: Partial<HTMLImageElement>;
+			i: Partial<HTMLElement>;
+			button: Partial<HTMLButtonElement>;
+			br: Partial<HTMLBRElement>;
+			select: Partial<HTMLSelectElement>;
+			option: Partial<HTMLOptionElement>;
+			ul: Partial<HTMLUListElement>;
+			li: Partial<HTMLLIElement>;
+		}
+
+		interface Element extends HTMLElement {}
+		interface IntrinsicClassAttributes<T> {
+			[_: string]: any;
+		}
+		/*
+	interface HtmlElementInstance { }
+	interface ElementAttributesProperty { __props: any; }
+	interface ElementTypeProperty { __elementType: any; }
+	*/
+	}
 }
 
 export default Component;
