3e293ddb9f03671882c53026d90309075bbcf519
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-10-15 23:16:29 +0200

    Add configuration for production builds.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,7 +3,11 @@ const Paths = require("./paths");
 const Webpack = require("webpack");
 const merge = require("webpack-merge");
 
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const BaseConfig = require("./webpack.common");
 
@@ -11,19 +15,30 @@ module.exports = merge(BaseConfig, {
 	entry: {
 		webfun: Path.resolve(Paths.sourceRoot, "app/main")
 	},
-	devtool: "inline-source-map",
-	mode: "development",
+	mode: "production",
 	output: {
 		filename: "[name].js",
 		path: Paths.buildRoot
 	},
-	serve: {
-		content: [Paths.projectRoot, Paths.assetsRoot, Path.resolve(Paths.sourceRoot, "app")]
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: true // set to true if you want JS source maps
+			}),
+			new OptimizeCSSAssetsPlugin({})
+		]
 	},
 	plugins: [
+		new CleanWebpackPlugin([Paths.buildRoot], { root: Paths.projectRoot }),
 		new HtmlWebpackPlugin({
 			template: Path.resolve(Paths.sourceRoot, "./app/index.html"),
-			title: "WebFun Development"
+			title: "WebFun"
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
 		})
 	],
 	module: {
@@ -32,15 +47,8 @@ module.exports = merge(BaseConfig, {
 				test: /\.scss$/,
 				exclude: /node_modules/,
 				use: [
-					{
-						loader: "style-loader"
-					},
-					{
-						loader: "css-loader",
-						options: {
-							minimize: true
-						}
-					},
+					{ loader: MiniCssExtractPlugin.loader },
+					{ loader: "css-loader" },
 					{
 						loader: "sass-loader",
 						options: {
