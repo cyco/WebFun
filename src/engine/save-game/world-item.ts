294534a54964e7b0ace37ecc1b0c4e0da5c7186a
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-11-10 11:08:26 +0100

    Save game inspector can launch a game from the current state.

--- src/engine/save-game/world-item.ts
+++ src/engine/save-game/world-item.ts
@@ -1,18 +1,18 @@
 class WorldItem {
-	public visited: boolean;
-	public solved_1: number;
-	public solved_3: number;
-	public solved_2: number;
-	public solved_4: number;
-	public zoneId: number;
-	public field_C: number;
-	public required_item_id: number;
-	public find_item_id: number;
-	public field_Ea: number;
-	public additionalRequiredItem: number;
-	public field_16: number;
-	public npc_id: number;
-	public zoneType: number;
+	public visited: boolean = false;
+	public solved_1: number = -1;
+	public solved_3: number = -1;
+	public solved_2: number = -1;
+	public solved_4: number = -1;
+	public zoneId: number = -1;
+	public field_C: number = -1;
+	public required_item_id: number = -1;
+	public find_item_id: number = -1;
+	public field_Ea: number = -1;
+	public additionalRequiredItem: number = -1;
+	public field_16: number = -1;
+	public npc_id: number = -1;
+	public zoneType: number = 0;
 }
 
 export default WorldItem;
