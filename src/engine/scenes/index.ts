28c8ee26e8714271bad6b138ccc9df50a6d55ec0
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-11-10 19:57:35 +0100

    Implement lose scene.

--- src/engine/scenes/index.ts
+++ src/engine/scenes/index.ts
@@ -6,5 +6,16 @@ import Scene from "./scene";
 import SpeechScene from "./speech-scene";
 import TransitionScene from "./transition-scene";
 import ZoneScene from "./zone-scene";
+import LoseScene from "./lose-scene";
 
-export { DetonatorScene, MapScene, PauseScene, PickupScene, Scene, SpeechScene, TransitionScene, ZoneScene };
+export {
+	LoseScene,
+	DetonatorScene,
+	MapScene,
+	PauseScene,
+	PickupScene,
+	Scene,
+	SpeechScene,
+	TransitionScene,
+	ZoneScene
+};
--- src/engine/scenes/lose-scene.ts
+++ src/engine/scenes/lose-scene.ts
@@ -1,10 +1,41 @@
 import { Renderer } from "../rendering";
+import ZoneScene from "./zone-scene";
 import Scene from "./scene";
+import { ZoneType } from "src/engine/objects";
 
 class LoseScene extends Scene {
-	async update(ticks: number) {}
+	private _zoneScene: ZoneScene;
 
-	render(renderer: Renderer) {}
+	public willShow(): void {
+		this._zoneScene = new ZoneScene();
+		this.engine.hero.visible = false;
+		this._zoneScene.engine = this.engine;
+		this._zoneScene.zone = this.engine.data.zones.withType(ZoneType.Lose).first();
+		this._zoneScene.willShow();
+	}
+
+	public didShow(): void {
+		this._zoneScene.didShow();
+		this.engine.metronome.stop();
+	}
+
+	async update(_: number) {}
+
+	render(renderer: Renderer) {
+		this._zoneScene.render(renderer);
+	}
+
+	public willHide(): void {
+		this._zoneScene.willHide();
+	}
+
+	public didHide(): void {
+		this._zoneScene.didHide();
+	}
+
+	public isOpaque() {
+		return false;
+	}
 }
 
 export default LoseScene;
