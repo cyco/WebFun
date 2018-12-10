4b9045bff5d4b96e2b45fe1492fc70985644ff1a
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-12-10 18:32:26 +0100

    Implement search for zone picker.

--- src/debug/components/zone-filter.ts
+++ src/debug/components/zone-filter.ts
@@ -1,3 +1,39 @@
-class ZoneFilter {}
+import { SearchDelegate } from "src/ui/components/list";
+import { Zone, ZoneType } from "src/engine/objects";
+
+class ZoneFilter implements SearchDelegate<Zone, RegExp[]> {
+	private _cache: { [_: number]: string } = {};
+
+	prepareListSearch(searchValue: string): RegExp[] {
+		return searchValue.split(" ").map(s => new RegExp(s, "i"));
+	}
+
+	includeListItem(searchValue: RegExp[], tile: Zone): boolean {
+		if (!tile) return true;
+
+		const string = this._stringForZone(tile);
+		return searchValue.every(r => r.test(string));
+	}
+
+	private _stringForZone(zone: Zone): string {
+		const cachedValue = this._cache[zone.id];
+		if (cachedValue) return cachedValue;
+
+		const components: string[] = [
+			zone.name,
+			`${zone.id}`,
+			zone.type.name,
+			zone.planet.name,
+			zone.hasTeleporter ? "teleporter" : "",
+			...zone.puzzleNPCs.map(tile => tile.name),
+			...zone.requiredItems.map(tile => tile.name),
+			...zone.providedItems.map(tile => tile.name),
+			...zone.goalItems.map(tile => tile.name),
+			...zone.actions.map(actn => actn.name)
+		];
+
+		return (this._cache[zone.id] = components.unique().join(" "));
+	}
+}
 
 export default ZoneFilter;
