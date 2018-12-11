32ec58fb297479cc55d7c3719012ffea5f103a06
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-12-11 12:03:36 +0100

    Improve error handling in sound loader.

--- src/app/audio/dom-sound-loader.ts
+++ src/app/audio/dom-sound-loader.ts
@@ -9,14 +9,20 @@ class DOMSoundLoader extends SoundLoader<HTMLAudioElement> {
 
 	loadSound(file: string) {
 		return new Promise<HTMLAudioElement>((resolve, reject) => {
-			const url = this.baseURL + encodeURIComponent(file + ".mp3");
-			const audio = new Audio(url);
-			audio.load();
-			if (audio.readyState === 4) {
-				resolve(audio);
-			} else {
-				audio.addEventListener("loadeddata", () => resolve(audio));
-				audio.addEventListener("error", reject);
+			try {
+				const url = this.baseURL + encodeURIComponent(file + ".mp3");
+				const audio = new Audio(url);
+				if (audio.readyState === 4) {
+					resolve(audio);
+				} else {
+					audio.addEventListener("loadeddata", () => resolve(audio));
+					audio.addEventListener("error", reject);
+				}
+				for (let i = 0; i < 10; i++) {
+					audio.load();
+				}
+			} catch (e) {
+				reject(e);
 			}
 		});
 	}
