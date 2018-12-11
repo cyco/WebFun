7aa349484e94296af22a61b1d7e2935360c1b21a
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-12-11 11:00:15 +0100

    Add syntax definition for automated gameplay.

---
+++ src/debug/automation/syntax.tsx
@@ -0,0 +1,25 @@
+export default {
+	Move: {
+		West: "←",
+		East: "→",
+		North: "↑",
+		South: "↓",
+		NorthWest: "↖",
+		NorthEast: "↗",
+		SouthEast: "↘",
+		SouthWest: "↙"
+	},
+	Drag: {
+		West: "⇠",
+		East: "⇢",
+		North: "⇡",
+		South: "⇣"
+	},
+	Place: {
+		Start: "「",
+		End: "」"
+	},
+	Attack: "🔫",
+	Comment: "-",
+	Wait: "."
+};
