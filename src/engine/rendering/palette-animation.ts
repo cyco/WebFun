3f9913baace7d4d780239abc09bd8d23c8b77385
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2018-12-18 20:43:10 +0100

    Implement color cycling rules.

--- src/engine/rendering/palette-animation.ts
+++ src/engine/rendering/palette-animation.ts
@@ -3,20 +3,47 @@ import ColorPalette from "./color-palette";
 class PaletteAnimation {
 	private _original: ColorPalette;
 	private _current: ColorPalette;
-	private _state: number = 0;
+	private _mutable: Uint32Array;
+	private _state: boolean = true;
 
 	public constructor(palette: ColorPalette) {
 		this._original = palette;
 		this._current = palette.slice();
+		this._mutable = new Uint32Array(this._current.buffer);
 	}
 
-	reset() {
+	public reset() {
 		this._current = this.original.slice();
-		this._state = 0;
+		this._mutable = new Uint32Array(this._current.buffer);
+		this._state = true;
 	}
 
-	step(distance: number = 1) {
-		this._state += distance;
+	public step() {
+		// normal cycles
+		this.cycle(10, 5);
+		this.cycle(202, 1);
+		this.cycle(204, 1);
+		this.cycle(206, 1);
+		this.cycle(224, 4);
+		this.cycle(238, 5);
+
+		// the remaining colors are animated slower
+		if ((this._state = !this._state)) return;
+
+		this.cycle(198, 1);
+		this.cycle(200, 1);
+		this.cycle(229, 8);
+		this.cycle(215, 8);
+		this.cycle(244, 1);
+	}
+
+	private cycle(start: number, count: number) {
+		const end = this._mutable[start + count];
+		do {
+			this._mutable[start + count] = this._mutable[start + count - 1];
+		} while (--count);
+
+		this._mutable[start] = end;
 	}
 
 	get current(): ColorPalette {
