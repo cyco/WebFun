2e23cb46240dc28609caa9f0e0a374563d529074
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-01-06 14:31:29 +0100

    Show tile name as tooltip.

--- src/debug/components/tile-view.ts
+++ src/debug/components/tile-view.ts
@@ -3,5 +3,14 @@ import "./tile-view.scss";
 
 class TileView extends AbstractTileView {
 	public static readonly tagName = "wf-debug-tile-view";
+
+	set tile(t) {
+		super.tile = t;
+		this.title = t ? t.name : "";
+	}
+
+	get tile() {
+		return super.tile;
+	}
 }
 export default TileView;
