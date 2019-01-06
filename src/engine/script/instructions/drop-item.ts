a856fc2d5793c750685b593bf3e89f23f4e21157
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-01-06 17:42:05 +0100

    Add fallbacks for puzzle items to item-related instructions.

--- src/engine/script/instructions/drop-item.ts
+++ src/engine/script/instructions/drop-item.ts
@@ -9,12 +9,22 @@ import InstructionType from "../instruction";
 export default <InstructionType>{
 	Opcode: 0x1b,
 	Arguments: [Type.TileID, Type.ZoneX, Type.ZoneY],
-	Implementation: async (instruction: Instruction, engine: Engine, _: Action): Promise<Result> => {
+	Implementation: async (instruction: Instruction, engine: Engine, action: Action): Promise<Result> => {
 		const args = instruction.arguments;
+		let itemID = args[0];
+
+		if (itemID === -1) {
+			const worldLocation = engine.currentWorld.locationOfZone(action.zone);
+			if (!worldLocation) console.warn("can find location of zone", action.zone, "on current world");
+			const worldItem = engine.currentWorld.at(worldLocation);
+			itemID = worldItem.findItem.id;
+			action.zone.solved = true;
+		}
 
 		const pickupScene = new PickupScene(engine);
 		pickupScene.location = new Point(args[1], args[2]);
-		pickupScene.tile = engine.data.tiles[args[0]];
+		pickupScene.tile = engine.data.tiles[itemID];
+
 		engine.sceneManager.pushScene(pickupScene);
 
 		return Result.Void;
