4f283d9ca20d8dc42b592d324287cb53c7786026
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-01-06 18:57:16 +0100

    Also mark main zone as solved when instruction is executed in a room.

--- src/engine/script/instructions/mark-as-solved.ts
+++ src/engine/script/instructions/mark-as-solved.ts
@@ -7,8 +7,14 @@ import InstructionType from "../instruction";
 export default <InstructionType>{
 	Opcode: 0x1e,
 	Arguments: [],
-	Implementation: async (_: Instruction, _engine: Engine, action: Action): Promise<Result> => {
+	Implementation: async (_: Instruction, engine: Engine, action: Action): Promise<Result> => {
 		action.zone.solved = true;
+
+		const worldLocation = engine.currentWorld.locationOfZone(action.zone);
+		if (!worldLocation) console.warn("can find location of zone", action.zone, "on current world");
+		const worldItem = engine.currentWorld.at(worldLocation);
+		worldItem.zone.solved = true;
+
 		return Result.Void;
 	}
 };
