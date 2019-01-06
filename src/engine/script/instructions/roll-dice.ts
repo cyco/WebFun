764d6b2baf9c826a5f112d27fc54f13542da9b41
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-01-06 14:57:54 +0100

    Fix roll-dice instrution to respect lower bound of 1.

--- src/engine/script/instructions/roll-dice.ts
+++ src/engine/script/instructions/roll-dice.ts
@@ -7,14 +7,11 @@ import InstructionType from "../instruction";
 export default <InstructionType>{
 	Opcode: 0x0c,
 	Arguments: [Type.Number],
-	Description:
-		"Set current zone's `random` to a random value between 0 and `arg_0`. _TODO: verify this isn't $0 < random <= arg_0$.",
-	Implementation: async (instruction: Instruction, engine: Engine, action: Action): Promise<Result> => {
-		//  zone->random = rand() % instruction->arg1 + 1
-		// TODO: consider using { rand } from '/util'
+	Description: "Set current zone's `random` to a random value between 1 and `arg_0`.",
+	Implementation: async (instruction: Instruction, _: Engine, action: Action): Promise<Result> => {
 		const args = instruction.arguments;
 		const zone = action.zone;
-		zone.random = Math.round(Math.random() * args[0]) % args[0];
+		zone.random = 1 + (Math.round(Math.random() * args[0]) % args[0]);
 		return Result.Void;
 	}
 };
