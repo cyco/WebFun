8880e0c57f5c77fc198a03889276dd45fa297854
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-01-06 13:44:05 +0100

    Implement item placement.

--- src/engine/script/conditions/place-item.ts
+++ src/engine/script/conditions/place-item.ts
@@ -1,11 +1,31 @@
 import Engine from "../../engine";
 import Zone from "../../objects/zone";
 import { int16, Type } from "../types";
+import EvaluationMode from "../evaluation-mode";
 import Condition from "src/engine/script/condition";
 
 export default <Condition>{
 	Opcode: 0x03,
 	Arguments: [Type.Number, Type.Number, Type.Number, Type.Number, Type.Number],
-	Implementation: async (_: int16[], _zone: Zone, _engine: Engine): Promise<boolean> => false
-	// TODO: validate against original implementation
+	Implementation: async (
+		args: int16[],
+		zone: Zone,
+		engine: Engine,
+		mode: EvaluationMode
+	): Promise<boolean> => {
+		if (mode !== EvaluationMode.PlaceItem) return false;
+
+		const [x, y, z, target, item] = args;
+		if (x < 0 || y < 0 || z < 0 || target < 0) {
+			console.warn("Not implemented");
+			return false;
+		}
+
+		return (
+			engine.inputManager.placedTileLocation.x === x &&
+			engine.inputManager.placedTileLocation.y == y &&
+			engine.inputManager.placedTile.id === item &&
+			zone.getTileID(x, y, z) === target
+		);
+	}
 };
