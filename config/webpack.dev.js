058a8ac34e3297b637bb647d31cbef912308b630
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-04-06 09:46:13 +0200

    Remove circular-dependency webpack plugin

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,11 +5,6 @@ const Webpack = require("webpack");
 const merge = require("webpack-merge");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CircularDependencyPlugin = require("circular-dependency-plugin");
-
-const MAX_CYCLES = 5;
-let numCyclesDetected = 0;
-let skippableCycles = 4;
 
 const BaseConfig = require("./webpack.common");
 
@@ -42,26 +37,6 @@ module.exports = merge(BaseConfig, {
 		new HtmlWebpackPlugin({
 			template: Path.resolve(Paths.sourceRoot, "./app/index.html"),
 			title: "WebFun Development"
-		}),
-		new CircularDependencyPlugin({
-			onStart({ compilation }) {
-				numCyclesDetected = 0;
-				skippableCycles = 4;
-			},
-			onDetected({ module: webpackModuleRecord, paths, compilation }) {
-				numCyclesDetected++;
-				if (numCyclesDetected >= skippableCycles) process.stdout.write(paths.join(" -> ") + "\n\n\n");
-				compilation.warnings.push(new Error(paths.join(" -> ")));
-			},
-			onEnd({ compilation }) {
-				if (numCyclesDetected - skippableCycles > MAX_CYCLES) {
-					compilation.errors.push(
-						new Error(
-							`Detected ${numCyclesDetected} cycles which exceeds configured limit of ${MAX_CYCLES}.`
-						)
-					);
-				}
-			}
 		})
 	],
 	module: {
