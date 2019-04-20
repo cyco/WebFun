ef2b5e2b91f8c29b573c15a8f67cf533ad090415
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-04-20 13:40:18 +0200

    Animate bullets

--- src/engine/objects/char-frame.ts
+++ src/engine/objects/char-frame.ts
@@ -1,5 +1,16 @@
 import Tile from "src/engine/objects/tile";
 
+export const enum CharFrameEntry {
+	Up = 0,
+	Down = 1,
+	ExtensionUp = 2,
+	Left = 3,
+	ExtensionDown = 4,
+	ExtensionLeft = 5,
+	Right = 6,
+	ExtensionRight = 7
+}
+
 class CharFrame {
 	private _tiles: Tile[];
 
@@ -12,35 +23,35 @@ class CharFrame {
 	}
 
 	get up() {
-		return this._tiles[0];
+		return this._tiles[CharFrameEntry.Up];
 	}
 
 	get down() {
-		return this._tiles[1];
+		return this._tiles[CharFrameEntry.Down];
 	}
 
 	get extensionUp() {
-		return this._tiles[2];
+		return this._tiles[CharFrameEntry.ExtensionUp];
 	}
 
 	get left() {
-		return this._tiles[3];
+		return this._tiles[CharFrameEntry.Left];
 	}
 
 	get extensionDown() {
-		return this._tiles[4];
+		return this._tiles[CharFrameEntry.ExtensionDown];
 	}
 
 	get extensionLeft() {
-		return this._tiles[5];
+		return this._tiles[CharFrameEntry.ExtensionLeft];
 	}
 
 	get right() {
-		return this._tiles[6];
+		return this._tiles[CharFrameEntry.Right];
 	}
 
 	get extensionRight() {
-		return this._tiles[7];
+		return this._tiles[CharFrameEntry.ExtensionRight];
 	}
 }
 
