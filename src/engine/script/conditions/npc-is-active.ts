2de4bfed81155a932736c687be31bfb32da07330
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-04-25 23:50:14 +0200

    Check for NPC's `enabled` attribute in script conditions

--- src/engine/script/conditions/npc-is-active.ts
+++ src/engine/script/conditions/npc-is-active.ts
@@ -8,5 +8,5 @@ export default {
 	Arguments: [Type.NPCID],
 	Description: "True if npc `arg_0` is alive and well.",
 	Implementation: async (args: int16[], zone: Zone, _: Engine): Promise<boolean> =>
-		args[0] >= 0 && args[0] <= zone.npcs.length && zone.npcs[args[0]] && zone.npcs[args[0]].id !== -1 // TODO: might be npc.enabled
+		args[0] >= 0 && args[0] <= zone.npcs.length && zone.npcs[args[0]] && zone.npcs[args[0]].enabled
 };
