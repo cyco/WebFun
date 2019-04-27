d3d7cceb60ca9773400e6b3692978a21750b3c00
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-04-27 16:45:58 +0200

    Use dropItem shorthand on engine instead of constructing new pickip scenes by hand

--- src/engine/script/instructions/drop-item.ts
+++ src/engine/script/instructions/drop-item.ts
@@ -3,32 +3,25 @@ import { Result, Type } from "../types";
 import Action from "../../objects/action";
 import Engine from "../../engine";
 import Instruction from "../../objects/instruction";
-
-import { PickupScene } from "src/engine/scenes";
 import { Point } from "src/util";
 
 export default {
 	Opcode: 0x1b,
 	Arguments: [Type.TileID, Type.ZoneX, Type.ZoneY],
 	Implementation: async (instruction: Instruction, engine: Engine, action: Action): Promise<Result> => {
-		const args = instruction.arguments;
-		let itemID = args[0];
+		let [itemId, x, y] = instruction.arguments;
 
-		if (itemID === -1) {
+		if (itemId === -1) {
 			const worldLocation = engine.currentWorld.locationOfZone(action.zone);
 			if (!worldLocation) console.warn("can't find location of zone", action.zone, "on current world");
 			const worldItem = engine.currentWorld.at(worldLocation);
-			itemID = worldItem.findItem.id;
+			itemId = worldItem.findItem.id;
 			action.zone.solved = true;
 			worldItem.zone.solved = true;
 		}
 
-		const pickupScene = new PickupScene(engine);
-		pickupScene.location = new Point(args[1], args[2]);
-		pickupScene.tile = engine.data.tiles[itemID];
+		engine.dropItem(engine.data.tiles[itemId], new Point(x, y));
 
-		engine.sceneManager.pushScene(pickupScene);
-
-		return Result.Void;
+		return Result.UpdateText;
 	}
 };
