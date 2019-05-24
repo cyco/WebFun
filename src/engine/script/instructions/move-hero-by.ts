5714b1530d2e55921daff36f367907c4efa43902
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-05-24 23:15:39 +0200

    Enhance opcode 0x13 to accept relative and absolute movement coordinates

--- src/engine/script/instructions/move-hero-by.ts
+++ src/engine/script/instructions/move-hero-by.ts
@@ -8,8 +8,13 @@ export default {
 	Opcode: 0x13,
 	Arguments: [Type.Number, Type.Number, Type.Number, Type.Number, Type.Number],
 	Implementation: async (instruction: Instruction, engine: Engine, _: Action): Promise<Result> => {
-		engine.hero.location.x += instruction.arguments[0];
-		engine.hero.location.y += instruction.arguments[1];
+		const [relx, rely, absx, absy, _absz] = instruction.arguments;
+
+		engine.hero.location.x += relx;
+		if (absx !== -1) engine.hero.location.x = absx;
+
+		engine.hero.location.y += rely;
+		if (absy !== -1) engine.hero.location.y = absy;
 
 		// original implementation actually has a hard break here
 		return Result.Void;
