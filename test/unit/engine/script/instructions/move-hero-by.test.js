98ddfb29d95fcfa1227cbb83f5cda2792df1435f
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-05-24 23:15:39 +0200

    Enhance opcode 0x13 to accept relative and absolute movement coordinates

--- test/unit/engine/script/instructions/move-hero-by.test.js
+++ test/unit/engine/script/instructions/move-hero-by.test.js
@@ -8,12 +8,27 @@ describeInstruction("MoveHeroBy", (execute, engine) => {
 
 		const instruction = new Instruction({});
 		instruction._opcode = MoveHeroBy.Opcode;
-		instruction._arguments = [-1, 1];
+		instruction._arguments = [-1, 2];
 
 		await execute(instruction);
 
 		expect(engine.hero.location.x).toBe(2);
 		expect(engine.hero.location.y).toBe(3);
+
+		done();
+	});
+
+	it("can set the heros position to an absolute value", async done => {
+		engine.hero.location = new Point(3, 2);
+
+		const instruction = new Instruction({});
+		instruction._opcode = MoveHeroBy.Opcode;
+		instruction._arguments = [-1, -1, 4, 7];
+
+		await execute(instruction);
+
+		expect(engine.hero.location.x).toBe(4);
+		expect(engine.hero.location.y).toBe(7);
 
 		done();
 	});
