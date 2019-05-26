c89331b08a5932fee1ed1baf5cbd8ec02a4d32ed
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-05-26 19:31:51 +0200

    Implement `win-game` opcode

--- src/engine/script/instructions/win-game.ts
+++ src/engine/script/instructions/win-game.ts
@@ -1,13 +1,15 @@
 import Action from "../../objects/action";
 import Engine from "../../engine";
 import Instruction from "../../objects/instruction";
+import { WinScene } from "src/engine/scenes";
 
 import { Result } from "../types";
 
 export default {
 	Opcode: 0x1f,
 	Arguments: [],
-	Implementation: async (_: Instruction, _engine: Engine, _action: Action): Promise<Result> => {
-		throw "Game Won!";
+	Implementation: async (_: Instruction, engine: Engine, _action: Action): Promise<Result> => {
+		engine.sceneManager.pushScene(new WinScene());
+		return Result.UpdateScene;
 	}
 };
