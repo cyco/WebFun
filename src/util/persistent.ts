8239766c71ff501f5eb96df40d1c73347592db37
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-05-30 00:50:08 +0200

    Simplify persistence proxy

--- src/util/persistent.ts
+++ src/util/persistent.ts
@@ -5,14 +5,16 @@ function persistent<T extends object>(object: T, prefix: string = null, storage:
 
 	return new Proxy(object, {
 		get(target: T, p: PropertyKey, _: any): any {
-			if (typeof p === "string")
-				return storage.has(buildKey(p)) ? storage.load(buildKey(p)) : (target as any)[p];
-			return (target as any)[p];
+			console.assert(typeof p === "string");
+			const key = p as string;
+			return storage.has(buildKey(key)) ? storage.load(buildKey(key)) : (target as any)[key];
 		},
 
 		set(target: T, p: PropertyKey, value: any, _: any): boolean {
-			if (typeof p === "string") storage.store(buildKey(p), value);
-			else (target as any)[p] = value;
+			console.assert(typeof p === "string");
+			const key = p as string;
+			storage.store(buildKey(key), value);
+			(target as any)[key] = value;
 			return true;
 		}
 	});
