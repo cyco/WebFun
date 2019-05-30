118bb43e7220dc467440fd593083acf8403d8683
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-05-30 13:24:28 +0200

    Add test for equipable items

--- test/unit/engine/type/type.test.js
+++ test/unit/engine/type/type.test.js
@@ -1,3 +1,24 @@
-xdescribe("Missing test", () => {
-	it("must still be written", () => {});
+import { GameType } from "src/engine/type";
+
+describe("WebFun.Engine.GameType", () => {
+	it("holds information specific to each game", () => {
+		expect(GameType).toBeClass();
+	});
+
+	it("allow for items to be equipped if they are weapons", () => {
+		const mockType = new (class extends GameType {})();
+		const mockWeapon = {
+			isWeapon() {
+				return true;
+			}
+		};
+		const mockItem = {
+			isWeapon() {
+				return false;
+			}
+		};
+
+		expect(mockType.canBeEquipped(mockWeapon)).toBeTrue();
+		expect(mockType.canBeEquipped(mockItem)).toBeFalse();
+	});
 });
