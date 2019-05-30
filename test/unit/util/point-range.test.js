a27ebb374fe5be745c57818bdb3f7e4aadad2dac
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-05-30 12:10:11 +0200

    Test defaults and backwards search in `PointRange´

--- test/unit/util/point-range.test.js
+++ test/unit/util/point-range.test.js
@@ -22,9 +22,7 @@ describe("WebFun.Util.PointRange", () => {
 		let visited = {};
 		const range = new PointRange(new Point(3, 6), new Point(4, 7));
 
-		range.iterate(function(point) {
-			visited[point.x + "x" + point.y] = true;
-		}, new Point(0, 1));
+		range.iterate(point => (visited[point.x + "x" + point.y] = true), new Point(0, 1));
 
 		expect(visited).toEqual({
 			"3x6": true,
@@ -33,9 +31,7 @@ describe("WebFun.Util.PointRange", () => {
 
 		visited = {};
 
-		range.iterate(function(point) {
-			visited[point.x + "x" + point.y] = true;
-		}, new Point(1, 0));
+		range.iterate(point => (visited[point.x + "x" + point.y] = true), new Point(1, 0));
 
 		expect(visited).toEqual({
 			"3x6": true,
@@ -47,11 +43,44 @@ describe("WebFun.Util.PointRange", () => {
 		const visited = {};
 		const range = new PointRange(new Point(3, 6), new Point(4, 7));
 
-		range.iterate(function(point, control) {
+		range.iterate((point, control) => {
 			visited[point.x + "x" + point.y] = true;
 			control.stop = true;
 		}, new Point(0, 1));
 
 		expect(Object.keys(visited).length).toBe(1);
+	});
+
+	it("by default it iterates diagonally", () => {
+		const visited = {};
+		const range = new PointRange(new Point(3, 6), new Point(5, 7));
+
+		range.iterate(point => (visited[point.x + "x" + point.y] = true));
+		expect(Object.keys(visited).length).toBe(2);
+	});
+
+	it("can start iterating at the end", () => {
+		const visited = {};
+		const range = new PointRange(new Point(5, 7), new Point(3, 6));
+
+		range.iterate(
+			(point, ctrl) => ((visited[point.x + "x" + point.y] = true), (ctrl.stop = true)),
+			new Point(-1, -1)
+		);
+		expect(visited["5x7"]).toBe(true);
+	});
+
+	it("searches diagonally by default", () => {
+		const range = new PointRange(new Point(3, 5), new Point(10, 10));
+		const result = range.find(point => point.x % 2 === 0 && point.y % 2 === 0);
+		expect(result.x).toBe(4);
+		expect(result.y).toBe(6);
+	});
+
+	it("can search backwards", () => {
+		const range = new PointRange(new Point(9, 9), new Point(3, 5));
+		const result = range.find(point => point.x % 2 === 0 && point.y % 2 === 0, new Point(-1, -1));
+		expect(result.x).toBe(8);
+		expect(result.y).toBe(8);
 	});
 });
