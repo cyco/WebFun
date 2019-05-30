acaaa00341879b7947a7ba7de91b26ec8fe2f580
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-05-30 13:24:28 +0200

    Add test for equipable items

--- src/std/dom.ts
+++ src/std/dom.ts
@@ -2,35 +2,6 @@ import { global } from "./index";
 
 const window = global.window;
 
-if (!global.Storage)
-	global.Storage = class {
-		get length() {
-			return Object.keys(this).length;
-		}
-
-		clear(): void {
-			Object.keys(this).forEach(k => {
-				delete this[k];
-			});
-		}
-
-		getItem(key: string): string | null {
-			return this[key];
-		}
-
-		key(index: number): string | null {
-			return Object.keys(this)[index];
-		}
-		removeItem(key: string): void {
-			delete this[key];
-		}
-
-		setItem(key: string, value: string): void {
-			this[key] = value;
-		}
-		[name: string]: any;
-	};
-
 export const {
 	Storage,
 	XMLHttpRequest,
