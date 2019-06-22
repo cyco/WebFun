5609da01725c1782a84af83886ca94bb2516b0d3
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-06-22 21:18:08 +0200

    Remove zone's required item if instruction does not specify which item to remove

--- src/engine/script/instructions/remove-item.ts
+++ src/engine/script/instructions/remove-item.ts
@@ -8,9 +8,13 @@ export default {
 	Opcode: 0x1d,
 	Arguments: [Type.TileID],
 	Description: "Remove one instance of item `arg_0` from the inventory",
-	Implementation: async (instruction: Instruction, engine: Engine, _: Action): Promise<Result> => {
+	Implementation: async (instruction: Instruction, engine: Engine, action: Action): Promise<Result> => {
 		const args = instruction.arguments;
-		const item = engine.assetManager.get(Tile, args[0]);
+		const zone = action.zone;
+		const worldItem = engine.currentWorld.itemForZone(zone);
+
+		const itemID = args[0] < 0 ? worldItem.requiredItem.id : args[0];
+		const item = engine.assetManager.get(Tile, itemID);
 		engine.inventory.removeItem(item);
 
 		return Result.Void;
