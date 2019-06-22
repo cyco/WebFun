7670b9055effabb822250339baae4aeb5c5d1315
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-06-22 14:07:32 +0200

    Build real story when testing full world gameplay

--- test/acceptance/simulations/index.test.ts
+++ test/acceptance/simulations/index.test.ts
@@ -3,8 +3,6 @@ import run from "test/helpers/gameplay/test-runner";
 
 var testFiles = require.context("./", false, /\.wftest$/);
 
-describe("WebFun.Simulation", () => {
-	testFiles.keys().forEach((key: string) => {
-		run(key, testFiles(key).default);
-	});
+testFiles.keys().forEach((key: string) => {
+	run(key, testFiles(key).default);
 });
