028a7063602bbd1ef048417356667d2edfbddaa3
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-06-22 21:38:31 +0200

    Add expectation to check if a game is won

---
+++ src/debug/automation/test/expectations/story-solved.ts
@@ -0,0 +1,21 @@
+import Expectation from "../expectation";
+import GameplayContext from "../gameplay-context";
+import { GameState } from "src/engine";
+
+class StorySolvedExpectation implements Expectation {
+	public static CanBeBuiltFrom(value: string) {
+		return value.contains("story") && value.contains("solved");
+	}
+
+	public static BuildFrom(_: IteratorResult<string>): StorySolvedExpectation {
+		return new StorySolvedExpectation();
+	}
+
+	evaluate(ctx: GameplayContext) {
+		it(`the story is solved`, () => {
+			expect(ctx.engine.gameState).toBe(GameState.Won);
+		});
+	}
+}
+
+export default StorySolvedExpectation;
