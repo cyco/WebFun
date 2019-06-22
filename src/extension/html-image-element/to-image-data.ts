6974072719bc076f46d5ee29538a763be4f143a3
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-06-22 23:41:25 +0200

    Define `toImageData` extension on HTMLImageElement

---
+++ src/extension/html-image-element/to-image-data.ts
@@ -0,0 +1,17 @@
+import { HTMLImageElement } from "src/std/dom";
+
+const toImageData = function(pixelated: boolean = true) {
+	const canvas = document.createElement("canvas");
+	canvas.width = this.width;
+	canvas.height = this.height;
+	canvas.classList.add("pixelated");
+
+	const ctx = canvas.getContext("2d");
+	if (pixelated) ctx.imageSmoothingEnabled = false;
+	ctx.drawImage(this, 0, 0);
+	return ctx.getImageData(0, 0, canvas.width, canvas.height);
+};
+
+HTMLImageElement.prototype.toImageData = HTMLImageElement.prototype.toImageData || toImageData;
+
+export default toImageData;
