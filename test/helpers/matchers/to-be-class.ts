a623a4fe0576fd4f580e5f6029ea419a9ece7eb0
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-06-23 11:33:43 +0200

    Add types for custom matchers

--- test/helpers/matchers/to-be-class.ts
+++ test/helpers/matchers/to-be-class.ts
@@ -1,9 +1,18 @@
 import addMatchers from "add-matchers";
 
-const isNativeClass = thing =>
+const isNativeClass = (thing: any) =>
 	typeof thing === "function" && thing.hasOwnProperty("prototype") && !thing.hasOwnProperty("arguments");
 
 addMatchers({
 	toBeClass: isNativeClass,
 	toBeAClass: isNativeClass
 });
+
+declare global {
+	namespace jasmine {
+		interface Matchers<T> {
+			toBeClass(): boolean;
+			toBeAClass(): boolean;
+		}
+	}
+}
--- test/helpers/matchers/to-be-instance-of.ts
+++ test/helpers/matchers/to-be-instance-of.ts
@@ -3,3 +3,11 @@ import addMatchers from "add-matchers";
 addMatchers({
 	toBeInstanceOf: (received, actual) => actual instanceof received
 });
+
+declare global {
+	namespace jasmine {
+		interface Matchers<T> {
+			toBeInstanceOf(thing: any): boolean;
+		}
+	}
+}
