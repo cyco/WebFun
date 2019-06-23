97198250591f1acd641d204bc2da7035bbac321e
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-06-23 12:28:30 +0200

    Add missing test for `sleep`

--- test/unit/util/sleep.test.ts
+++ test/unit/util/sleep.test.ts
@@ -1,3 +1,13 @@
-xdescribe("Missing test", () => {
-	it("must still be written", () => {});
+import * as stdlib from "src/std";
+import sleep from "src/util/sleep";
+declare var jasmine: any;
+
+describe("WebFun.Util.sleep", () => {
+	it("is an asynchronous function that resolves after a given time", () => {
+		spyOn(stdlib, "setTimeout");
+
+		sleep(10);
+		expect(stdlib.setTimeout).toHaveBeenCalledWith(jasmine.anything(), 10);
+		stdlib.setTimeout.calls.mostRecent().args[0]();
+	});
 });
