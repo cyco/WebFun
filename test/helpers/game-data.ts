15818ba73ae41d800e61fc3d67fa55090d6f016c
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-07-04 20:43:01 +0200

    Remove unused callback from game data test helper

--- test/helpers/game-data.ts
+++ test/helpers/game-data.ts
@@ -3,21 +3,19 @@ import { readGameDataFile, GameTypeYoda, GameType } from "src/engine";
 import { InputStream } from "src/util";
 import { Data } from "src/engine/file-format";
 
-export default (type: GameType, callback?: (data: Data) => void): Promise<Data> => {
+export default (type: GameType, path?: string): Promise<Data> => {
 	return new Promise(async resolve => {
-		const file = type === GameTypeYoda ? "yoda.data" : "indy.data";
+		const file = path ? path : type === GameTypeYoda ? "yoda.data" : "indy.data";
 		const contents = await getFixtureData(file);
 
 		if (!contents) {
 			console.warn(`Game data could not be found.`);
-			callback && callback(null);
 			resolve(null);
 			return;
 		}
 
 		const stream = new InputStream(contents);
 		const result = readGameDataFile(stream, type);
-		callback && callback(result);
 		resolve(result);
 	});
 };
