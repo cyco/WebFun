bda6e49a22f88d8821847ab885dacea0e17f0603
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-07-25 23:44:31 +0200

    Clean up configurations

--- config/webpack.node.js
+++
@@ -1,8 +0,0 @@
-const NodeExternals = require("webpack-node-externals");
-const BaseConfig = require("./webpack.common");
-const merge = require("webpack-merge");
-
-module.exports = merge(BaseConfig, {
-	target: "node",
-	externals: [NodeExternals()]
-});
