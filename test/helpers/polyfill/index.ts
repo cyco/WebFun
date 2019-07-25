bf6aa48f62e7b6e662be91faa5d1221a41b1f54f
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-07-25 23:34:27 +0200

    Remove unused polyfills

--- test/helpers/polyfill/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./dom";
-export * from "./storage";
-export * from "./local-storage";
-export * from "./image-data";
-export * from "./animation-frame";
-export * from "./performance";
