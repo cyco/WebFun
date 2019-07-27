faf75529b73b318d48e560b24bb46ba41c5094f4
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-07-27 16:28:13 +0200

    Implement patrol movement type

--- src/engine/npc-move/patrol.ts
+++ src/engine/npc-move/patrol.ts
@@ -13,23 +13,19 @@ export default (npc: NPC, zone: Zone, engine: Engine) => {
 	const tickCount = engine.metronome.tickCount;
 	const hero = engine.hero.location;
 
-	let direction: Point;
-	// TODO: v95 = (char *)npc_1 + 8 * npc_1.facingDirection;
-	const someX = 0; // TODO: x_11 = *((_DWORD *)v95 + 18);
-	const someY = 0; // TODO: y14 = *((_DWORD *)v95 + 19);
+	const waypoint = npc.patrolPath[npc.facingDirection + 1];
 	if (npc.cooldown) {
 		npc.cooldown--;
 		return noMovement(npc, zone, engine);
 	}
 	if (even(tickCount)) return noMovement(npc, zone, engine);
-	if (someX === npc.x) direction.x = 0;
-	else direction.x = someX <= npc.x ? -1 : 1;
-	if (someY === npc.y) direction.y = 0;
-	else direction.y = someY <= npc.y ? -1 : 1;
+	const direction = waypoint.comparedTo(npc.position);
+
 	if (!direction.x && !direction.y) {
 		npc.facingDirection++;
 		if (npc.facingDirection > 2) npc.facingDirection = -1;
 	}
+
 	if (canPerformMeleeAttack(direction, npc, hero)) {
 		performMeleeAttack(npc, engine);
 		return noMovement(npc, zone, engine);
