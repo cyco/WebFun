c6baca4f40cd599d0c979fac25cf19af1f5ba4cd
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-08-27 19:41:04 +0200

    Remove tests for standard library exports

--- test/unit/std/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { setTimeout } from "src/std";
-
-describe("std", () => {
-	it("is used to import classes and functions found in the standard library", () => {
-		expect(setTimeout).toBeFunction();
-	});
-});
