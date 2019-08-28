82d7f23a925e0110608edaf52ab6d8380acecc38
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-08-28 21:41:07 +0200

    Allow inferrable types in eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
 		"@typescript-eslint/no-empty-interface": "off",
 		"@typescript-eslint/no-var-requires": "off",
 		"@typescript-eslint/no-namespace": "off",
+		"@typescript-eslint/no-inferrable-types": "off",
 		// temporarily disabled:
 		"sort-imports": "off",
 		"@typescript-eslint/explicit-member-accessibility": "off",
