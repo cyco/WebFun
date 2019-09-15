9cf45f450f58f03d6e88464f012d4e2bf7bd4492
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-09-15 14:03:10 +0200

    Clean up instructions

--- src/engine/script/instructions/set-counter.ts
+++ src/engine/script/instructions/set-counter.ts
@@ -7,7 +7,7 @@ export default {
 	Opcode: 0x0d,
 	Arguments: [Type.Number],
 	Description: "Set current zone's `counter` value to a `arg_0`",
-	Implementation: async (instruction: Instruction, engine: Engine, action: Action): Promise<Result> => {
+	Implementation: async (instruction: Instruction, _: Engine, action: Action): Promise<Result> => {
 		action.zone.counter = instruction.arguments[0];
 
 		return Result.Void;
