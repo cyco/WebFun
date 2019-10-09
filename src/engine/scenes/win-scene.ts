ecb578ad08340e8570bdd82a7ac94f2a3df324ef
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2019-10-09 20:18:47 +0200

    Implement win scene

--- src/engine/scenes/win-scene.ts
+++ src/engine/scenes/win-scene.ts
@@ -1,10 +1,66 @@
 import { Renderer } from "../rendering";
 import Scene from "./scene";
+import ZoneScene from "./zone-scene";
+import { Zone } from "src/engine/objects";
+import { Point } from "src/util";
+import CanvasRenderer from "src/app/rendering/canvas/canvas-renderer";
+import { round } from "src/std/math";
 
 class WinScene extends Scene {
-	async update(_: number) {}
+	private _zoneScene: ZoneScene;
+	public readonly score: string;
+	private _ticksUntilScoreIsShown = 11;
+	private _originalLocation: Point = new Point(0, 0);
 
-	render(_: Renderer) {}
+	public constructor(score: number) {
+		super();
+
+		this.score = round(score).toString(10);
+	}
+
+	public willShow(): void {
+		const engine = this.engine;
+
+		this._originalLocation = engine.hero.location;
+		engine.hero.visible = false;
+		engine.hero.location = new Point(0, 0);
+		engine.camera.update(0);
+
+		this._zoneScene = new ZoneScene(
+			engine,
+			engine.assets.find(Zone, ({ type }) => type === Zone.Type.Win)
+		);
+		this._zoneScene.willShow();
+	}
+
+	public didShow(): void {
+		this.engine.temporaryState.justEntered = true;
+		this._zoneScene.didShow();
+	}
+
+	async update(ticks: number) {
+		this._ticksUntilScoreIsShown -= 1;
+		this._zoneScene.update(ticks);
+	}
+
+	render(renderer: Renderer) {
+		this._zoneScene.render(renderer);
+
+		if (this._ticksUntilScoreIsShown > 0) return;
+		if (!(renderer instanceof CanvasRenderer)) return;
+		renderer.renderText(this.score, new Point(201, 239), { textAlign: "center" });
+	}
+
+	public willHide(): void {
+		this._zoneScene.willHide();
+
+		this.engine.hero.location = this._originalLocation;
+		this.engine.camera.update(0);
+	}
+
+	public didHide(): void {
+		this._zoneScene.didHide();
+	}
 }
 
 export default WinScene;
