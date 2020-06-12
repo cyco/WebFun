973082266411d630513e0b28bbaf75654666c29a
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2020-06-12 18:28:37 +0200

    Preserve sourcemaps when adjusting module exports

--- config/spy-on-imports-webpack-plugin.js
+++ config/spy-on-imports-webpack-plugin.js
@@ -1,4 +1,4 @@
-const {OriginalSource} = require('webpack-sources');
+const { ReplaceSource } = require('webpack-sources');
 
 class SpyOnImportsWebpackPlugin {
 	apply(compiler) {
@@ -7,24 +7,23 @@ class SpyOnImportsWebpackPlugin {
 				chunk.files.forEach(filename => {
 					const search = /exports, "([a-z0-9A-Z_]+)", \{\n\s+enumerable: true,\n\s+get: function get\(\) \{\n\s+return ([a-z0-9A-Z_]+)\.([a-z0-9A-Z_]+)/g
 
-					let source = compilation.assets[filename].source();
+					const source = new ReplaceSource(compilation.assets[filename], filename)
+					const sourceCode = source.source();
 					let result = null;
-					while ((result = search.exec(source)) !== null) {
+					while ((result = search.exec(sourceCode)) !== null) {
 						const index = result.index;
 						const exportName = result[1];
 						const moduleName = result[2];
 						const importName = result[3];
 					
-						const prefix = source.slice(0, result.index);
-						const suffix = source.slice(result.index + result[0].length);
 						const newContent = 'exports, "'+exportName+'", {\nenumerable: true,set: function(v) { '+moduleName+'.'+importName+' = v; },\nget: function get() {\nreturn '+moduleName+'.'+importName;
 
-						source = prefix + newContent + suffix;
+						source.replace(result.index, result.index + result[0].length, newContent)
 					}
 
 					compilation.updateAsset(
 						filename,
-						old => new OriginalSource(source, filename)
+						old => source
 					);
 				});
 			});
