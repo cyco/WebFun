859ebd0e2c9cc4d6989b165a1db3e57981d6fd32
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2021-01-18 14:23:56 +0100

    Implement character reference resolver

--- src/app/editor/reference/resolvers/char-resolver.ts
+++ src/app/editor/reference/resolvers/char-resolver.ts
@@ -13,6 +13,28 @@ class CharResolver implements ResolverInterface<Char> {
 	public resolve(needle: Char, op = equal): ReferencesTo<Char> {
 		const result: ReferencesTo<Char> = [];
 
+		for (const character of this.data.characters) {
+			if (op(character.id, needle.id)) {
+				result.push({ from: character, to: character, via: ["id"] });
+			}
+
+			if (
+				!character.isWeapon() &&
+				character.reference !== -1 &&
+				op(character.reference, needle.id)
+			) {
+				result.push({ from: character, to: needle, via: ["weapon"] });
+			}
+		}
+
+		for (const zone of this.data.zones) {
+			for (const monster of zone.monsters) {
+				if (op(monster.face.id, needle.id)) {
+					result.push({ from: monster, to: needle, via: [zone] });
+				}
+			}
+		}
+
 		return result;
 	}
 }
