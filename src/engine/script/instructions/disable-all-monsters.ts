ab58d05fcdcf73636a4586ea9e8a20ad6cbc7fdd
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2021-03-02 13:03:57 +0100

    chore: Remove unused special case

--- src/engine/script/instructions/disable-all-monsters.ts
+++ src/engine/script/instructions/disable-all-monsters.ts
@@ -1,24 +1,14 @@
 import Engine from "../../engine";
-import { Action, Instruction, Zone } from "src/engine/objects";
+import { Action, Instruction } from "src/engine/objects";
 
 import { Result } from "../types";
-import { Point, Direction } from "src/util";
 
 export default {
 	Opcode: 0x1a,
 	Arguments: [],
 	Description: "Disable all monsters",
 	Implementation: async (_: Instruction, _engine: Engine, action: Action): Promise<Result> => {
-		const zone = action.zone;
-
-		zone.monsters.forEach(monster => {
-			const position = new Point(monster.position.x, monster.position.y, Zone.Layer.Object);
-			const currentTile = zone.getTile(position);
-			if (currentTile && currentTile === monster.face.getFace(Direction.South, -1)) {
-				zone.setTile(null, position);
-			}
-			monster.enabled = false;
-		});
+		action.zone.monsters.forEach(monster => (monster.enabled = false));
 
 		return Result.Void;
 	}
