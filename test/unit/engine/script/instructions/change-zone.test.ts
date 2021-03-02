85e4aac47c93e9d11f063b291bfde06120b06e18
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2021-03-02 13:24:40 +0100

    chore: Add test to ensure that transition scenes do not overlap

--- test/unit/engine/script/instructions/change-zone.test.ts
+++ test/unit/engine/script/instructions/change-zone.test.ts
@@ -3,18 +3,32 @@ import ChangeZone from "src/engine/script/instructions/change-zone";
 import { RoomTransitionScene } from "src/engine/scenes";
 
 describeInstruction("ChangeZone", (execute, engine) => {
-	it("Switches to a different zone using the room animation", async () => {
-		const zone = {};
-		engine.assets.populate(Zone, [null, null, zone, null]);
+	let mockZone: Zone;
+	beforeEach(() => {
+		mockZone = {} as any;
+		engine.assets.populate(Zone, [null, null, mockZone, null]);
 		engine.sceneManager = {
-			pushScene: jasmine.createSpy("pushScene")
+			pushScene: jasmine.createSpy(),
+			popScene: jasmine.createSpy()
 		};
 		spyOn(engine, "findLocationOfZone").and.returnValue({});
+	});
 
+	it("Switches to a different zone using the room animation", async () => {
 		const instruction = { opcode: ChangeZone.Opcode, arguments: [2] };
 
 		await execute(instruction);
 
 		expect(engine.sceneManager.pushScene).toHaveBeenCalledWith(jasmine.any(RoomTransitionScene));
+	});
+
+	it("ensures that no two transition scenes overlap", async () => {
+		engine.sceneManager.currentScene = new RoomTransitionScene();
+
+		const instruction = { opcode: ChangeZone.Opcode, arguments: [2] };
+
+		await execute(instruction);
+
+		expect(engine.sceneManager.popScene).toHaveBeenCalledTimes(1);
 	});
 });
