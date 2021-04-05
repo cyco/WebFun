289399cd8beabd86814102478f834a6bce8cd7f5
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2021-04-05 18:15:06 +0200

    chore: Use singular for `cursor` directory

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -195,7 +195,7 @@ module.exports = {
 			},
 			{
 				test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
-				exclude: /cursors/,
+				exclude: /cursor/,
 				use: [
 					{
 						loader: "file-loader",
