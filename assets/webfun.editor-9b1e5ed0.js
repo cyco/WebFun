34dab6b8e6abb4163ee6b29415326b1aa8fe6e75
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2021-04-28 10:35:17 +0200

    chore: Update release

--- assets/webfun.editor-9b1e5ed0.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a987e541fed0eda52e2404a6710eec9cd8864350c6437282e4193affaf73446c
-size 227107
--- assets/webfun.vendor-9b1e5ed0.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f36e289582361233a6d920f409e32622fe429006a059d3385a5ba3370021e38a
-size 113972
