7b552486831436b2b7ff6fa94d8b4a0ca006d376
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2021-06-29 11:48:17 +0200

    chore: Include travel destination when simulating a travel start zone

--- src/app/webfun/debug/components/test-creator/adjacent-zones.ts
+++ src/app/webfun/debug/components/test-creator/adjacent-zones.ts
@@ -1,10 +1,19 @@
 import { srand } from "src/util";
-import { Zone } from "src/engine/objects";
+import { Zone, Hotspot } from "src/engine/objects";
 
 export default (zone: Zone, zones: Zone[]): Zone[] => {
 	srand(zone.id);
-	return zones
+	const adjacentZones = zones
 		.slice()
 		.filter((z: Zone) => z !== zone && z.type === Zone.Type.Empty && z.planet === zone.planet)
 		.shuffle();
+
+	if (zone.type === Zone.Type.TravelStart) {
+		zone.hotspots
+			.filter(htsp => htsp.type === Hotspot.Type.VehicleTo && htsp.arg !== -1)
+			.map(htsp => htsp.arg)
+			.forEach((id, idx) => (adjacentZones[idx] = zones[id]));
+	}
+
+	return adjacentZones;
 };
